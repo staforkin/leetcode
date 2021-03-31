1a73bf1d7d523e6d4a5e8218cc37eafd1b80d279
staforkin <staforkin.denis@gmail.com>
2021-04-01 00:06:42 +0300

    MaxPathSum problem

---
+++ NumberToWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberToWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(NumberToWordsImpl(1234));
+        }
+
+        public static string NumberToWordsImpl(int num)
+        {
+            return num.ToString();
+        }
+    }
+}
