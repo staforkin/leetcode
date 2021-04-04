9ce6897d8f2e1b3cd8af645d9d80fe58781c1c0d
staforkin <staforkin.denis@gmail.com>
2021-04-04 17:11:39 +0300

    WordDictionary problem solved

---
+++ WordDictionary/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new WordDictionaryClass();
+            dict.AddWord("a");
+            dict.AddWord("a");
+            Console.WriteLine(dict.Search("aa"));
+        }
+    }
+
+    public class WordDictionaryClass
+    {
+        TrieNode trie;
+
+        public WordDictionaryClass()
+        {
+            trie = new TrieNode();
+        }
+
+        public void AddWord(string word)
+        {
+            TrieNode node = trie;
+            foreach (var c in word.ToCharArray())
+            {
+                if (!node.Children.ContainsKey(c))
+                {
+                    node.Children.Add(c, new TrieNode());
+                }
+                node = node.Children[c];
+            }
+            node.Word = true;
+        }
+
+        public bool Search(string word)
+        {
+            return SearchInNode(word, trie);
+        }
+
+        private bool SearchInNode(string word, TrieNode node)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                var c = word[i];
+                if (!node.Children.ContainsKey(c))
+                {
+                    if (c == '.')
+                    {
+                        foreach (var key in node.Children.Keys)
+                        {
+                            TrieNode child = node.Children[key];
+                            if (SearchInNode(word.Substring(i + 1), child))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    node = node.Children[c];
+                }
+            }
+            return node.Word;
+        }
+    }
+
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> Children = new Dictionary<char, TrieNode>();
+        public bool Word = false;
+        public TrieNode() { }
+    }
+}
