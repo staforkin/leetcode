10ec2d6dac7750d2076003a4d6708bdfcba4d710
staforkin <staforkin.denis@gmail.com>
2021-04-06 20:39:53 +0300

    TreeTodoublyList problem

---
+++ TreeTodoublyList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TreeTodoublyList
+{
+    class Program
+    {
+        static Node first = null;
+        static Node last = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static Node TreeToDoublyListImpl(Node root)
+        {
+            first = null;
+            last = null;
+            if (root == null)
+                return null;
+            Helper(root);
+            last.right = first;
+            first.left = last;
+            return first;
+        }
+
+        private static void Helper(Node node)
+        {
+            if (node != null)
+            {
+                //left
+                Helper(node.left);
+                if (last != null)
+                {
+                    last.right = node;
+                    node.left = last;
+                }
+                else
+                {
+                    first = node;
+                }
+                last = node;
+                Helper(node.right);
+            }
+        }
+    }
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node() { }
+        public Node(int _val)
+        {
+            val = _val;
+            left = null;
+            right = null;
+        }
+        public Node(int _val, Node _left, Node _right)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+}
