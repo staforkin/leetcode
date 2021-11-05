8eb5554183fc0f5430e295df1c4b1cca328cc539
staforkin <staforkin.denis@gmail.com>
2021-11-05 16:06:57 +0300

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ MaxLength/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] { "aa", "bcefgyh" };
+            Console.WriteLine(MaxLength(arr));
+        }
+        public static int MaxLength(IList<string> arr)
+        {
+            List<string> res = new List<string>();
+            res.Add("");
+            foreach(var str in arr){
+                if(!IsAllUnique(str.ToCharArray())) continue;
+                List<string> resList = new List<string>();
+                foreach (var candidate in res)
+                {
+                    var t = candidate+str;
+                    if(IsAllUnique(t.ToCharArray())) resList.Add(t);
+                }
+                res.AddRange(resList);
+            }
+            int answer = 0;
+            foreach(var s in res){
+                answer = Math.Max(answer, s.Length);
+            }
+            return answer;
+        }
+
+        static bool IsAllUnique(char[] chars)
+        {
+            var frequency = new int[26];
+            foreach (var c in chars)
+            {
+                ++frequency[c - 'a'];
+                if (frequency[c - 'a'] > 1)
+                { return false; }
+            }
+            return true;
+        }
+    }
+}
