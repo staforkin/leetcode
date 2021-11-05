5f0bd6af1fdf1468eb6b4ecc1ddb3c2c84d8ec3b
staforkin <staforkin.denis@gmail.com>
2021-11-05 16:23:22 +0300

    1304. Find N Unique Integers Sum up to Zero

---
+++ SumZero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            Console.WriteLine(string.Join(";", SumZero(n)));
+        }
+
+        public static int[] SumZero(int n)
+        {
+            var res = new int[n];
+            int left = 0, right = n-1, start = 1;
+            while(left<right){
+                res[left++]=start;
+                res[right--]=-start;
+                start++;
+            }
+            return res;
+        }
+    }
+}
