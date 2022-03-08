01aa60c20b92e6d60840f5324700d268e8d91b01
staforkin <staforkin.denis@gmail.com>
2022-03-08 16:41:56 +0300

    reverse-integer problem

---
+++ Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Reverse
+{
+    class Program
+    {
+        //https://leetcode.com/problems/reverse-integer/
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.Reverse(-123));
+        }
+    }
+
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            //123456
+            var sb = new StringBuilder();
+            if(x<0){
+                sb.Append("-");
+                x*=(-1);
+            }
+            do
+            {
+                sb.Append(x % 10);
+                x = x / 10;
+            } while (x > 0);
+            int res = 0;
+            int.TryParse(sb.ToString(), out res);
+            return res;
+        }
+    }
+}
