8d8adff936e1b573b615be11875e54d0c18ad198
staforkin <staforkin.denis@gmail.com>
2022-03-10 23:03:18 +0300

    3sum problem

---
+++ ThreeSum/Program.cs
@@ -0,0 +1,42 @@
+﻿var solution = new Solution();
+foreach (var s in solution.ThreeSum(new[] { -1, 0, 1, 2, -1, -4 }))
+{
+    Console.WriteLine(string.Join(',', s));
+}
+
+//https://leetcode.com/problems/3sum
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var res = new List<IList<int>>();
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            int j = i + 1;
+            int k = nums.Length - 1;
+            while (j < k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                {
+                    res.Add(new List<int>() { nums[i], nums[j], nums[k] });
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                }
+                else if (sum > 0)
+                {
+                    k--;
+                }
+                else if (sum < 0)
+                {
+                    j++;
+                }
+            }
+        }
+        return res.ToArray();
+    }
+}
