996f421b46af59ee86b36c532f92675af82c2d13
staforkin <staforkin.denis@gmail.com>
2022-03-12 19:36:46 +0300

    valid-parentheses problem

---
+++ IsValid/Program.cs
@@ -0,0 +1,48 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsValid("(([[{}]]))"));
+
+//https://leetcode.com/problems/valid-parentheses/
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(s[i]);
+            }
+            else
+            {
+                switch (stack.Peek())
+                {
+                    case '(':
+                        if(s[i]==')')
+                            stack.Pop();
+                        else
+                            stack.Push(s[i]);
+                        break;
+                    case '{':
+                        if(s[i]=='}')
+                            stack.Pop();
+                        else
+                            stack.Push(s[i]);
+                        break;
+                    case '[':
+                        if(s[i]==']')
+                            stack.Pop();
+                        else
+                            stack.Push(s[i]);
+                        break;
+                    default:
+                        stack.Push(s[i]);
+                        break;
+                }
+            }
+
+        }
+        return stack.Count == 0;
+    }
+}
