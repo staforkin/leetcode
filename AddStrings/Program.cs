173d05665ab96be0079f6d3c29b2312efbd9c7e6
staforkin <staforkin.denis@gmail.com>
2022-03-13 13:35:59 +0300

    add-strings problem cleaned up

--- AddStrings/Program.cs
+++ AddStrings/Program.cs
@@ -5,17 +5,18 @@ namespace AddStrings
 {
     class Program
     {
+        //https://leetcode.com/problems/add-strings/
         static void Main(string[] args)
         {
-            Console.WriteLine(AddStringsImpl("999","1"));
+            var solution = new Solution();
+            Console.WriteLine(solution.AddStrings("999", "1"));
         }
+    }
 
-        private static string AddStringsImpl(string num1, string num2)
+    public class Solution
+    {
+        public string AddStrings(string num1, string num2)
         {
-            //i    
-            //9 9 9 - t1 - biggest
-            //    1 - t2 - smalles
-            //j   
             var sb = new StringBuilder();
             string t1 = num1.Length >= num2.Length ? num1 : num2;
             string t2 = num1.Length >= num2.Length ? num2 : num1;
@@ -23,24 +24,30 @@ namespace AddStrings
             var l1 = t1.Length;
             var l2 = t2.Length;
             int overflow = 0;
-            int j = l2-1;
-            for (int i = l1-1;i > -2; i--)
+            int j = l2 - 1;
+            for (int i = l1 - 1; i > -2; i--)
             {
                 int ii = 0;
                 int jj = 0;
-                if(i>-1){
-                    ii = int.Parse(new[]{t1[i]});
-                } else if(overflow==0){
+                if (i > -1)
+                {
+                    ii = int.Parse(new[] { t1[i] });
+                }
+                else if (overflow == 0)
+                {
                     break;
                 }
-                if(j>-1){
-                    jj = int.Parse(new[]{t2[j]});
-                }                
-                int tt = ii+jj+overflow;
-                if(tt>9){
+                if (j > -1)
+                {
+                    jj = int.Parse(new[] { t2[j] });
+                }
+                int tt = ii + jj + overflow;
+                if (tt > 9)
+                {
                     overflow = 1;
-                    tt=tt-10;
-                } else{ overflow = 0;}
+                    tt = tt - 10;
+                }
+                else { overflow = 0; }
                 sb.Append(tt);
                 j--;
             }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ List of problems solved:
     - [Remove Nth node from end of list](https://leetcode.com/problems/remove-nth-node-from-end-of-list)
     - [Merge two sorted lists](https://leetcode.com/problems/merge-two-sorted-lists)
     - [Add binary](https://leetcode.com/problems/add-binary)
+    - [Add strings](https://leetcode.com/problems/add-strings)
