abe9d9fc5ea16be9c379e1bafe7e8979e6763441
staforkin <staforkin.denis@gmail.com>
2022-11-07 23:19:26 +0300

    IsAnagram problem

---
+++ IsAnagram/Program.cs
@@ -0,0 +1,13 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsAnagram("anagram", "nagaram"));
+
+//https://leetcode.com/problems/valid-anagram
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        var sArr = s.ToCharArray();
+        var tArr = t.ToCharArray();
+        Array.Sort(sArr);
+        Array.Sort(tArr);
+        return Enumerable.SequenceEqual(sArr, tArr);
+    }
+}
