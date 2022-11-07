a474dab77da593abc7e4d76f94484a8484e79409
staforkin <staforkin.denis@gmail.com>
2022-11-07 23:19:01 +0300

    HammingWeight problem

---
+++ HammingWeight/Program.cs
@@ -0,0 +1,16 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.HammingWeight(11));
+
+Console.WriteLine(00000000000000000000000000001011);
+
+//https://leetcode.com/problems/number-of-1-bits/
+public class Solution {
+    public int HammingWeight(uint n) {
+        int ones = 0;
+        while(n>0){
+            ones = (ones + (int)(n & 1));
+            n = n>>1;
+        }
+        return ones;
+    }
+}
