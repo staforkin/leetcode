4db6d9543bd16eb6daa000649a41df664b5bab94
staforkin <staforkin.denis@gmail.com>
2022-11-10 21:41:21 +0300

    MaxProfit switched to net6

--- MaxProfit/Program.cs
+++ MaxProfit/Program.cs
@@ -1,45 +1,36 @@
-﻿using System;
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxProfit(new[]{7,1,5,3,6,4}));
 
-namespace max_profit
+public class Solution
 {
-    class Program
+    public int MaxProfit(int[] prices)
     {
-        static void Main(string[] args)
+        int result = 0;
+        bool hitEnd = false;
+        for (int i = 0; i < prices.Length - 1; i++)
         {
-            var prices = new[] { 1, 2, 3, 4, 5 };//4
-            int result = MaxProfit(prices);
-            Console.Write(result);
-        }
-
-        public static int MaxProfit(int[] prices)
-        {
-            int result = 0;
-            bool hitEnd = false;
-            for (int i = 0; i <  prices.Length-1; i++)
+            int delta = 0;
+            for (int j = i + 1; j < prices.Length; j++)
             {
-                int delta = 0;
-                for (int j = i + 1; j < prices.Length; j++)
+                int t = prices[j] - prices[i];
+                if (t > delta)
                 {
-                    int t = prices[j] - prices[i];
-                    if (t > delta)
-                    {
-                        delta = t;
-                    }
-                    else
-                    {
-                        i = j - 1;
-                        break;
-                    }
-                    if (j == prices.Length - 1)
-                    {
-                        hitEnd = true;
-                    }
+                    delta = t;
                 }
-                result += delta;
-                if (hitEnd)
-                { break; }
+                else
+                {
+                    i = j - 1;
+                    break;
+                }
+                if (j == prices.Length - 1)
+                {
+                    hitEnd = true;
+                }
             }
-            return result;
+            result += delta;
+            if (hitEnd)
+            { break; }
         }
+        return result;
     }
 }
