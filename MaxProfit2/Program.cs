cbbe61e8d8c7c310927d6f115039a81bc663aada
staforkin <staforkin.denis@gmail.com>
2022-11-10 21:42:24 +0300

    MaxProfit2 problem

---
+++ MaxProfit2/Program.cs
@@ -0,0 +1,17 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxProfit(new []{2,4,1}));
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        // 2 4 1
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.Length; i++) {
+            maxCur = Math.Max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.Max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
