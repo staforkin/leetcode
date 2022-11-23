b36398e24beb656a0ed93e224e3e87d870974ad1
staforkin <staforkin.denis@gmail.com>
2022-11-23 21:55:32 +0300

    setup for Construct Binary Tree from Preorder and Inorder Traversal

---
+++ BuildTree/Program.cs
@@ -0,0 +1,25 @@
+﻿var solution = new Solution();
+var preOrder = new []{3,9,20,15,7};
+var inOrder = new []{9,3,15,20,7};
+
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+public class Solution
+{
+    public TreeNode BuildTree(int[] preorder, int[] inorder)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
