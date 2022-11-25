e6d45378e339f6edf187e8434d1a7910788c7012
staforkin <staforkin.denis@gmail.com>
2022-11-25 20:57:51 +0300

    setup for LongestConsecutive

---
+++ LongestConsecutive/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LongestConsecutive(new[] { 100, 4, 200, 1, 3, 2 }));
+
+// https://leetcode.com/problems/longest-consecutive-sequence/
+public class Solution
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        return 0;
+    }
+}
