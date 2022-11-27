ed252af798c982f5f95cbbe90d5410a07ece03cf
staforkin <staforkin.denis@gmail.com>
2022-11-27 19:48:08 +0300

    Setup for Gas station problem

---
+++ CanCompleteCircuit/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CanCompleteCircuit(new[] { 1, 2, 3, 4, 5 }, new[] { 3, 4, 5, 1, 2 }));
+
+// https://leetcode.com/problems/gas-station/
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        
+    }
+}
