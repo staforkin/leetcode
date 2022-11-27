c1986279a10fff2cdfa10539c2ae63726d639f99
staforkin <staforkin.denis@gmail.com>
2022-11-27 20:30:01 +0300

    added second (fast) solution

--- CanCompleteCircuit/Program.cs
+++ CanCompleteCircuit/Program.cs
@@ -1,5 +1,5 @@
 ﻿var solution = new Solution();
-Console.WriteLine(solution.CanCompleteCircuit(new[] { 0,0,0,2 }, new[] { 0,0,1,0 }));
+Console.WriteLine(solution.CanCompleteCircuit2(new[] { 0, 0, 0, 2 }, new[] { 0, 0, 1, 0 }));
 
 // https://leetcode.com/problems/gas-station/
 public class Solution
@@ -38,5 +38,20 @@ public class Solution
             { return false; }
         }
         return true;
+    }
+
+    public int CanCompleteCircuit2(int[] gas, int[] cost)
+    {
+        int tank = 0;
+        int start = 0;
+        int total = 0;
+        for (int i = 0; i < gas.Length; i++)
+        {
+            if ((tank = tank + gas[i] - cost[i]) < 0)
+            {
+                start = i + 1; total += tank; tank = 0;
+            }
+        }
+        return (total + tank < 0) ? -1 : start;
     }
 }
