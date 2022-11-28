38f2e4ae3ffaaa46c4b07a3bb4cab9c52e1ccd81
staforkin <staforkin.denis@gmail.com>
2022-11-28 21:17:41 +0300

    setup for copy random list

---
+++ CopyRandomList/Program.cs
@@ -0,0 +1,24 @@
+﻿var solution = new Solution();
+
+
+public class Solution
+{
+    public Node CopyRandomList(Node head)
+    {
+
+    }
+}
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
