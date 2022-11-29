9309df18b2f53a702c1665b4b1ce5f7780805ae0
staforkin <staforkin.denis@gmail.com>
2022-11-29 19:48:06 +0300

    setup for word break problem

---
+++ WordBreak/Program.cs
@@ -0,0 +1,10 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.WordBreak("leetcode", new[] { "leet", "code" }));
+
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+
+    }
+}
