6df9ccc622d0f4cfa82273acbafbb42978b8010d
staforkin <staforkin.denis@gmail.com>
2022-11-30 19:02:01 +0300

    Setup for LRUCache problem

---
+++ LRUCache/Program.cs
@@ -0,0 +1,28 @@
+﻿var cache = new LRUCache(5);
+
+// https://leetcode.com/problems/lru-cache
+public class LRUCache
+{
+
+    public LRUCache(int capacity)
+    {
+
+    }
+
+    public int Get(int key)
+    {
+
+    }
+
+    public void Put(int key, int value)
+    {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
