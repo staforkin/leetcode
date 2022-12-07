4654fa458611b04bea89a9502a72f51c837c6839
staforkin <staforkin.denis@gmail.com>
2022-12-07 20:45:28 +0300

    Range sum of BST optimized solution added

--- RangeSumBST/Program.cs
+++ RangeSumBST/Program.cs
@@ -6,7 +6,7 @@ root.left.left = new TreeNode(3);
 root.left.right = new TreeNode(7);
 root.right.right = new TreeNode(18);
 
-Console.WriteLine(solution.RangeSumBST(root, 7, 15));
+Console.WriteLine(solution.RangeSumBST2(root, 7, 15));
 
 // https://leetcode.com/problems/range-sum-of-bst
 public class Solution
@@ -20,6 +20,24 @@ public class Solution
         return root.val >= low && root.val <= high
             ? root.val + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high)
             : RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+    }
+
+    public int RangeSumBST2(TreeNode root, int low, int high)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        if (root.val > high)
+        {
+            return RangeSumBST2(root.left, low, high);
+        }
+        else if (root.val < low)
+        {
+            return RangeSumBST2(root.right, low, high);
+        }
+
+        return root.val + RangeSumBST2(root.right, low, high) + RangeSumBST2(root.left, low, high);
     }
 }
 
