2f2f23a5af4ec5047980166cd4ed4ed14446146e
staforkin <staforkin.denis@gmail.com>
2022-12-13 21:12:05 +0300

    setup for MinFallingPathSum

---
+++ MinFallingPathSum/Program.cs
@@ -0,0 +1,16 @@
+﻿var solution = new Solution();
+var array = new int[][] {
+    new int[] { 2, 1, 3 },
+    new int[] { 6, 5, 4 },
+    new int[] { 7, 8, 9 }};
+
+Console.WriteLine(solution.MinFallingPathSum(array));
+
+// https://leetcode.com/problems/minimum-falling-path-sum
+public class Solution
+{
+    public int MinFallingPathSum(int[][] matrix)
+    {
+        return 0;
+    }
+}
