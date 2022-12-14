60e845ce3908b97be391c683a509889e729fee34
staforkin <staforkin.denis@gmail.com>
2022-12-14 20:12:06 +0300

    setup for house robber problem

---
+++ Rob/Program.cs
@@ -0,0 +1,10 @@
+﻿var solution = new Solution();
+var arr = new []{1,2,3,1};
+Console.WriteLine(solution.Rob(arr));
+
+// https://leetcode.com/problems/house-robber/
+public class Solution {
+    public int Rob(int[] nums) {
+        return 0;
+    }
+}
