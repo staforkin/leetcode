ee4c34c0c2fe924af1d8636a42591d0ae5ae7802
staforkin <staforkin.denis@gmail.com>
2022-12-15 21:49:19 +0300

    setup for LongestCommonSubsequence problem

---
+++ LongestCommonSubsequence/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LongestCommonSubsequence("abcde","ace"));
+
+// https://leetcode.com/problems/longest-common-subsequence/
+public class Solution {
+    public int LongestCommonSubsequence(string text1, string text2) {
+        
+    }
+}
