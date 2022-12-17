54802b301f84da96bc3355f4594ce4c036926c50
staforkin <staforkin.denis@gmail.com>
2022-12-17 18:39:51 +0700

    setup for Search Insert Position problem

---
+++ SearchInsert/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SearchInsert(new[] { 1, 3, 5, 6 }, 5));
+
+// https://leetcode.com/problems/search-insert-position
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+
+    }
+}
