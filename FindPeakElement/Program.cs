bfe636299a0a995d4a6870a3bc198dcc0458a211
staforkin <staforkin.denis@gmail.com>
2022-12-18 15:48:29 +0700

    setup for find peak element problem

---
+++ FindPeakElement/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindPeakElement(new []{1,2,4,5,1}));
+
+// https://leetcode.com/problems/find-peak-element
+public class Solution {
+    public int FindPeakElement(int[] nums) {
+        
+    }
+}
