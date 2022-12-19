75cac752e67efefe4af48c0af37ea5e95c39daeb
staforkin <staforkin.denis@gmail.com>
2022-12-19 16:33:23 +0700

    setup for Find if path exists in graph

---
+++ ValidPath/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+var n = 3;
+var arr = new int[][]{new []{0,1},new []{1,2},new []{2,3}, new []{3,0}};
+Console.WriteLine(solution.ValidPath(n,arr,1,23));
+
+// https://leetcode.com/problems/find-if-path-exists-in-graph
+public class Solution {
+    public bool ValidPath(int n, int[][] edges, int source, int destination) {
+        // 0 -> 1 -> 2 -> 3 -> 0
+    }
+}
