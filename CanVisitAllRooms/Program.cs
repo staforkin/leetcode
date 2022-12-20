e823831fe93a5ff392ee6ae32c8b970231277641
staforkin <staforkin.denis@gmail.com>
2022-12-20 17:16:11 +0700

    setup for CanVisitAllRooms problem

---
+++ CanVisitAllRooms/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var rooms = new List<IList<int>>() { new[] { 1 }, new[] { 2 }, new[] { 3 }, new int[0] { } };
+
+Console.WriteLine(solution.CanVisitAllRooms(rooms));
+// https://leetcode.com/problems/keys-and-rooms/
+public class Solution
+{
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        
+    }
+}
