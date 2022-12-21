668c7f4a65fe2cc5c0b72e05d72ee5bc007a912f
staforkin <staforkin.denis@gmail.com>
2022-12-21 17:51:41 +0700

    setup for Possible Bipartition problem

---
+++ PossibleBipartition/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+var arr = new int[][] { new[] { 1, 2 }, new[] { 1, 3 }, new[] { 2, 4 } };
+
+// https://leetcode.com/problems/possible-bipartition/
+public class Solution
+{
+    public bool PossibleBipartition(int n, int[][] dislikes)
+    {
+
+    }
+}
