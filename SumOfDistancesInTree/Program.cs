118dbd4890ba8e86673bd9e3765d706a9997c0bf
staforkin <staforkin.denis@gmail.com>
2022-12-22 14:02:31 +0700

    setup for SumOfDistancesInTree problem

---
+++ SumOfDistancesInTree/Program.cs
@@ -0,0 +1,15 @@
+﻿var solution = new Solution();
+var arr = new int[][]{new []{0,1},
+                        new []{0,2},
+                        new []{2,3},
+                        new []{2,4},
+                        new []{2,5}};
+
+// https://leetcode.com/problems/sum-of-distances-in-tree
+public class Solution
+{
+    public int[] SumOfDistancesInTree(int n, int[][] edges)
+    {
+
+    }
+}
