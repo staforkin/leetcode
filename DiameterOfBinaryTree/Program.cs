92b47212e55de4fa4d00d2630963cbe8c43b45fc
staforkin <staforkin.denis@gmail.com>
2022-12-24 15:48:36 +0700

    setup for DiameterOfBinaryTree problem

---
+++ DiameterOfBinaryTree/Program.cs
@@ -0,0 +1,26 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+// https://leetcode.com/problems/diameter-of-binary-tree/
+public class Solution {
+    public int DiameterOfBinaryTree(TreeNode root) {
+        
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
