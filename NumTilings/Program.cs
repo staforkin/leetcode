fc5e4d5c1202b9e48fa475043512b22c907beec5
staforkin <staforkin.denis@gmail.com>
2022-12-24 14:46:01 +0700

    setup for Domino and Tromino Tiling problem

---
+++ NumTilings/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumTilings(3));
+
+// https://leetcode.com/problems/domino-and-tromino-tiling/
+public class Solution {
+    public int NumTilings(int n) {
+        
+    }
+}
