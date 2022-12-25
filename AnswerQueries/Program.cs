74af21dcf1ba2647e66c91b8072ec0c0fc2343c3
staforkin <staforkin.denis@gmail.com>
2022-12-25 12:00:47 +0300

    setup for Longest Subsequence With Limited Sum problem

---
+++ AnswerQueries/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",",solution.AnswerQueries(new []{4,5,2,1}, new []{3,10,21})));
+
+// https://leetcode.com/problems/longest-subsequence-with-limited-sum
+public class Solution {
+    public int[] AnswerQueries(int[] nums, int[] queries) {
+        
+    }
+}
