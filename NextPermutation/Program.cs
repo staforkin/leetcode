40ec4e36e806bfba1fc0050dbf6fb33eb74d4f8c
staforkin <staforkin.denis@gmail.com>
2022-12-26 13:33:47 +0300

    setup for Next permutation problem

---
+++ NextPermutation/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var arr = new int[] { 1, 2, 3 };
+solution.NextPermutation(arr);
+Console.WriteLine(string.Join(",", arr));
+// https://leetcode.com/problems/next-permutation
+public class Solution
+{
+    public void NextPermutation(int[] nums)
+    {
+
+    }
+}
