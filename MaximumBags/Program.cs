c00da54ef9bddae57e75b8cb391dc892d4bee0eb
staforkin <staforkin.denis@gmail.com>
2022-12-27 16:03:26 +0300

    setup for Maximum Bags With Full Capacity of Rocks problem

---
+++ MaximumBags/Program.cs
@@ -0,0 +1,13 @@
+﻿var solution = new Solution();
+var capacity = new int[] { 2, 3, 4, 5 };
+var rocks = new int[] { 1, 2, 4, 4 };
+Console.WriteLine(solution.MaximumBags(capacity, rocks, 2));
+
+// https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks
+public class Solution
+{
+    public int MaximumBags(int[] capacity, int[] rocks, int additionalRocks)
+    {
+
+    }
+}
