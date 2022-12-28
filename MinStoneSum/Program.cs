be25cd27ca1274c73368ca0141f3b3de2a22d46a
staforkin <staforkin.denis@gmail.com>
2022-12-28 17:04:45 +0300

    setup for Remove Stones to Minimize the Total problem

---
+++ MinStoneSum/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinStoneSum(new[] { 5, 4, 9 }, 2));
+
+// https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+public class Solution
+{
+    public int MinStoneSum(int[] piles, int k)
+    {
+
+    }
+}
