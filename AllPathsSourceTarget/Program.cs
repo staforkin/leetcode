c1ec4167ba8c39ad0a0f009bd5b28e7ecde7ca50
staforkin <staforkin.denis@gmail.com>
2022-12-30 15:01:57 +0300

    Setup for All Paths From Source to Target problem

---
+++ AllPathsSourceTarget/Program.cs
@@ -0,0 +1,15 @@
+﻿var solution = new Solution();
+var arr = new int[][] { new int[] { 1, 2 }, new int[] { 3 }, new int[] { 3 }, new int[] { } };
+foreach (var item in solution.AllPathsSourceTarget(arr))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/all-paths-from-source-to-target
+public class Solution
+{
+    public IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+    {
+
+    }
+}
