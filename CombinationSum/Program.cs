44947cb74bd44985d645e70fad146a6e201610a6
staforkin <staforkin.denis@gmail.com>
2022-12-31 18:46:10 +0300

    setup for Combination Sum problem

---
+++ CombinationSum/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.CombinationSum(new[] { 2, 3, 5 }, 8))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/combination-sum/
+public class Solution
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+
+    }
+}
