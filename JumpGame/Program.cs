1a43a4608a5d7b8f6c3810246a4fe358083f6f14
staforkin <staforkin.denis@gmail.com>
2023-01-01 18:12:15 +0300

    setup for Jump Game 2

---
+++ JumpGame/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Jump(new[] { 2, 3, 1, 1, 4 }));
+
+// https://leetcode.com/problems/jump-game-ii
+public class Solution
+{
+    public int Jump(int[] nums)
+    {
+
+    }
+}
