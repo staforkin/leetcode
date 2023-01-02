42a4f7ce87af4d8637d8d8c05961f0795d0e2f9c
staforkin <staforkin.denis@gmail.com>
2023-01-02 13:51:28 +0300

    Setup for Detect Capital problem

---
+++ DetectCapitalUse/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.DetectCapitalUse("Google"));
+
+// https://leetcode.com/problems/detect-capital/
+public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+
+    }
+}
