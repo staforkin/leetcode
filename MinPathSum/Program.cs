cb21f072357c26919c384a2ba0987c541ee5883c
staforkin <staforkin.denis@gmail.com>
2023-01-04 15:19:12 +0300

    Setup for Minimum Path Sum problem

---
+++ MinPathSum/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var arr = new int[][] { new int[] { 1, 3, 1 }, new int[] { 1, 5, 1 }, new int[] { 4, 2, 1 } };
+Console.WriteLine(solution.MinPathSum(arr));
+
+// https://leetcode.com/problems/minimum-path-sum
+public class Solution
+{
+    public int MinPathSum(int[][] grid)
+    {
+
+    }
+}
