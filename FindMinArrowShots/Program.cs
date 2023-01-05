d6f30e841408d567054da7a52f9384ead0a1866d
staforkin <staforkin.denis@gmail.com>
2023-01-05 18:46:13 +0300

    Setup for Minimum Number of Arrows to Burst Balloons

---
+++ FindMinArrowShots/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+var arr = new int[][] { new int[] { 10, 16 }, new int[] { 2, 8 }, new int[] { 1, 6 }, new int[] { 7, 12 } };
+
+// https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons
+public class Solution
+{
+    public int FindMinArrowShots(int[][] points)
+    {
+        
+    }
+}
