167d7d3082782e2cd0fb16271b673fee9ea0fc76
staforkin <staforkin.denis@gmail.com>
2023-01-06 20:47:08 +0300

    Setup for Maximum Ice Cream Bars Problem

---
+++ MaxIceCream/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxIceCream(new int[] { 1, 3, 2, 4, 1 }, 7));
+
+// https://leetcode.com/problems/maximum-ice-cream-bars
+public class Solution
+{
+    public int MaxIceCream(int[] costs, int coins)
+    {
+
+    }
+}
