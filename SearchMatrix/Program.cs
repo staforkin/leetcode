4a3f07fe6bcf23a639c73d585109cdcdad5031a4
staforkin <staforkin.denis@gmail.com>
2023-01-07 13:54:32 +0300

    Setup for Search a 2D Matrix problem

---
+++ SearchMatrix/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+var matrix = new int[][]{new []{1,3,5,7},
+new []{10,11,16,20},
+new []{23,30,34,60}};
+Console.WriteLine(solution.SearchMatrix(matrix, 3));
+
+// https://leetcode.com/problems/search-a-2d-matrix
+public class Solution
+{
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+
+    }
+}
