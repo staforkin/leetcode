a1256f41540ee968eabcb27ab79771b8396e782b
staforkin <staforkin.denis@gmail.com>
2023-01-08 21:46:50 +0300

    Setup for Flatten Binary Tree to Linked List problem

---
+++ Flatten/Program.cs
@@ -0,0 +1,36 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(4);
+root.right = new TreeNode(5);
+root.right.right = new TreeNode(6);
+solution.Flatten(root);
+var t = root;
+while (t != null)
+{
+    Console.WriteLine(t.val);
+    t = t.right;
+}
+
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+public class Solution
+{
+    public void Flatten(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
