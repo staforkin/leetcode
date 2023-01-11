2ab915571d8525e604cb14fdf8a97641908bfd5b
staforkin <staforkin.denis@gmail.com>
2023-01-11 22:40:08 +0300

    Setup for Build Array from Permutation problem

---
+++ BuildArray/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.BuildArray(new[] { 0, 2, 1, 5, 3, 4 })));
+
+// https://leetcode.com/problems/build-array-from-permutation
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+
+    }
+}
