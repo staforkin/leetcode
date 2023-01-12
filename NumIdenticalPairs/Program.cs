0c842a1d438ad8bea0441e4f7bbf46d806a56c8f
staforkin <staforkin.denis@gmail.com>
2023-01-12 19:48:40 +0300

    Setup for Number of Good Pairs problem

---
+++ NumIdenticalPairs/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumIdenticalPairs(new[] { 1, 2, 3, 1, 1, 3 }));
+
+// https://leetcode.com/problems/number-of-good-pairs
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        
+    }
+}
