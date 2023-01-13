39f8176b3a663bca984845435e3c172aef0023f8
staforkin <staforkin.denis@gmail.com>
2023-01-13 19:43:44 +0300

    Setup for Palindrome Number problem

---
+++ IsPalindrome3/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsPalindrome(12321));
+
+// https://leetcode.com/problems/palindrome-number
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+
+    }
+}
