b541cc33226f14b877787205ccaa2d92bd244556
staforkin <staforkin.denis@gmail.com>
2023-01-14 13:07:26 +0300

    Setup for Lexicographically Smallest Equivalent String problem

---
+++ SmallestEquivalentString/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SmallestEquivalentString("parker", "morris", "parser"));
+
+// https://leetcode.com/problems/lexicographically-smallest-equivalent-string
+public class Solution
+{
+    public string SmallestEquivalentString(string s1, string s2, string baseStr)
+    {
+        
+    }
+}
