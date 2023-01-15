f3bce7cf5cc4afb63b8da7195ed3ee6369a5dade
staforkin <staforkin.denis@gmail.com>
2023-01-15 16:09:09 +0300

    Setup for First Letter to Appear Twice problem

---
+++ RepeatedCharacter/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RepeatedCharacter("abccbaacz"));
+
+// https://leetcode.com/problems/first-letter-to-appear-twice
+public class Solution
+{
+    public char RepeatedCharacter(string s)
+    {
+
+    }
+}
