cb1aae46cf3de03acde82ae9d89933cfb975049c
staforkin <staforkin.denis@gmail.com>
2023-01-16 20:35:59 +0300

    Setup for Insert Interval problem

---
+++ Insert/Program.cs
@@ -0,0 +1,15 @@
+﻿var solution = new Solution();
+var intervals = new int[][] { new int[] { 1, 3 }, new int[] { 6, 9 } };
+foreach (var item in solution.Insert(intervals, new int[] { 2, 5 }))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/insert-interval
+public class Solution
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+
+    }
+}
