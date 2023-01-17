525af214a9cf571d3473c6114f0631c5e2441de4
staforkin <staforkin.denis@gmail.com>
2023-01-17 20:43:40 +0300

    Setup for Flip String to Monotone Increasing problem

---
+++ MinFlipsMonoIncr/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinFlipsMonoIncr("0010110"));
+
+// https://leetcode.com/problems/flip-string-to-monotone-increasing
+public class Solution
+{
+    public int MinFlipsMonoIncr(string s)
+    {
+
+    }
+}
