9f791c145ddbb39258456077eccde36edd54d583
staforkin <staforkin.denis@gmail.com>
2023-01-18 23:05:55 +0300

    Setup for Maximum Sum Circular Subarray problem

---
+++ MaxSubarraySumCircular/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxSubarraySumCircular(new int[] { 5, -3, 5 }));
+
+// https://leetcode.com/problems/maximum-sum-circular-subarray
+public class Solution
+{
+    public int MaxSubarraySumCircular(int[] nums)
+    {
+        
+    }
+}
