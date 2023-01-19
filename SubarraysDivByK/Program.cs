50dca75ae50071400f2149ff5d4ba779aecc51b2
staforkin <staforkin.denis@gmail.com>
2023-01-19 19:52:11 +0300

    Setup for Subarray Sums Divisible by K problem

---
+++ SubarraysDivByK/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SubarraysDivByK(new[] { 4, 5, 0, -2, -3, 1 }, 5));
+
+// https://leetcode.com/problems/subarray-sums-divisible-by-k
+public class Solution
+{
+    public int SubarraysDivByK(int[] nums, int k)
+    {
+
+    }
+}
