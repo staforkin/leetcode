2d6aa8544610a0487ace8a2fcfe42f1d132e45a1
staforkin <staforkin.denis@gmail.com>
2023-01-21 17:31:56 +0300

    Setup for Restore IP Addresses problem

---
+++ RestoreIpAddresses/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.RestoreIpAddresses("25525511135"))
+{
+    Console.WriteLine(item);
+}
+
+// https://leetcode.com/problems/restore-ip-addresses
+public class Solution
+{
+    public IList<string> RestoreIpAddresses(string s)
+    {
+
+    }
+}
