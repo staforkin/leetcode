e7095c35fc370647e4e9e9afd8df4feff91e8a98
staforkin <staforkin.denis@gmail.com>
2023-01-22 15:39:42 +0300

    Setup for Combination Sum II problem

---
+++ CombinationSum2/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.CombinationSum2(new[] { 10, 1, 2, 7, 6, 1, 5 }, 8))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/combination-sum-ii
+public class Solution
+{
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+
+    }
+}
