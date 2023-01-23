35517e885aac4b72cc9579e70c7f48976bfff330
staforkin <staforkin.denis@gmail.com>
2023-01-23 20:16:43 +0300

    Setup for Find the Town Judge problem

---
+++ FindJudge/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var trust = new int[][] { new int[] { 1, 2 } };
+Console.WriteLine(solution.FindJudge(2, trust));
+
+//https://leetcode.com/problems/find-the-town-judge
+public class Solution
+{
+    public int FindJudge(int n, int[][] trust)
+    {
+
+    }
+}
