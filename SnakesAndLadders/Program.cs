996574a114ddc0f16d345f859eaba39c516f7f24
staforkin <staforkin.denis@gmail.com>
2023-01-24 21:07:03 +0300

    Setup for Snakes and Ladders problem

---
+++ SnakesAndLadders/Program.cs
@@ -0,0 +1,19 @@
+﻿var solution = new Solution();
+var board = new int[][]{
+    new int[]{-1,-1,-1,-1,-1,-1},
+    new int[]{-1,-1,-1,-1,-1,-1},
+    new int[]{-1,-1,-1,-1,-1,-1},
+    new int[]{-1,35,-1,-1,13,-1},
+    new int[]{-1,-1,-1,-1,-1,-1},
+    new int[]{-1,15,-1,-1,-1,-1}
+    };
+Console.WriteLine(solution.SnakesAndLadders(board));
+
+// https://leetcode.com/problems/snakes-and-ladders
+public class Solution
+{
+    public int SnakesAndLadders(int[][] board)
+    {
+
+    }
+}
