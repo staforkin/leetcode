a4af74597eb349d2bb3df437c025654f22d7e47c
staforkin <staforkin.denis@gmail.com>
2023-01-25 22:23:59 +0300

    Setup for Find Closest Node to Given Two Nodes problem

---
+++ ClosestMeetingNode/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ClosestMeetingNode(new[] { 2, 2, 3, -1 }, 0, 1));
+
+// https://leetcode.com/problems/find-closest-node-to-given-two-nodes
+public class Solution
+{
+    public int ClosestMeetingNode(int[] edges, int node1, int node2)
+    {
+
+    }
+}
