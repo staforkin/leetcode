0d089feb182df6497d5cfb38d6d2308013ce54d5
staforkin <staforkin.denis@gmail.com>
2023-01-27 19:53:40 +0300

    Setup for Concatenated Words problem

---
+++ FindAllConcatenatedWordsInADict/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.FindAllConcatenatedWordsInADict(new[] { "cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat" }))
+{
+    Console.WriteLine(item);
+}
+
+// https://leetcode.com/problems/concatenated-words
+public class Solution
+{
+    public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+    {
+
+    }
+}
