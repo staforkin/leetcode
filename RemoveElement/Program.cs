72d09722205b17cd598cb71fcb70aa6381a51f3a
staforkin <staforkin.denis@gmail.com>
2023-01-28 18:48:07 +0300

    Setip for Remove Element problem

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RemoveElement(new[] { 3, 2, 2, 3 }, 3));
+
+// https://leetcode.com/problems/remove-element
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+
+    }
+}
