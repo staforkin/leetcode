6c2a2dbe064edfcd6869d8f88871be1872101d17
staforkin <staforkin.denis@gmail.com>
2023-01-29 16:22:20 +0300

    Setup for Length of Last Word problem

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LengthOfLastWord("Hello World"));
+
+// https://leetcode.com/problems/length-of-last-word
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        
+    }
+}
