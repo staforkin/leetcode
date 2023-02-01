1a3c070108c6fde8bcd4f26351d759dee282b173
staforkin <staforkin.denis@gmail.com>
2023-02-01 21:35:52 +0300

    Setup for Minimum Depth of Binary Tree problem

---
+++ MinDepth/Program.cs
@@ -0,0 +1,29 @@
+﻿var solution = new Solution();
+var root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+Console.WriteLine(solution.MinDepth(root));
+
+// https://leetcode.com/problems/minimum-depth-of-binary-tree
+public class Solution
+{
+    public int MinDepth(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
