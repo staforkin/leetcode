140f486a99bd57dfdba85bca34871a9bae1c4dc4
staforkin <staforkin.denis@gmail.com>
2023-02-02 19:12:41 +0300

    Setup for Path Sum problem

---
+++ HasPathSum/Program.cs
@@ -0,0 +1,33 @@
+﻿var solution = new Solution();
+var root = new TreeNode(5);
+root.left = new TreeNode(4);
+root.left.left = new TreeNode(11);
+root.left.left.left = new TreeNode(7);
+root.left.left.right = new TreeNode(2);
+root.right = new TreeNode(8);
+root.right.left = new TreeNode(13);
+root.right.right = new TreeNode(4);
+root.right.right.right = new TreeNode(1);
+Console.WriteLine(solution.HasPathSum(root, 22));
+
+// https://leetcode.com/problems/path-sum
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
