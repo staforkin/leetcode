427800f7bd1d8b85ab4dd92bd9a7e65902ef24e5
staforkin <staforkin.denis@gmail.com>
2023-02-05 11:29:40 +0300

    Setup for Find All Anagrams in a String problem

---
+++ FindAnagrams/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FindAnagrams("cbaebabacd", "abc")));
+
+// https://leetcode.com/problems/find-all-anagrams-in-a-string
+public class Solution
+{
+    public IList<int> FindAnagrams(string s, string p)
+    {
+
+    }
+}
