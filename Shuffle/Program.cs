e50c55979842d742047c60880b53b5f5a631f7df
staforkin <staforkin.denis@gmail.com>
2023-02-06 19:34:35 +0300

    Setup for Shuffle the Array problem

---
+++ Shuffle/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(',', solution.Shuffle(new int[] { 2, 5, 1, 3, 4, 7 }, 3)));
+
+// https://leetcode.com/problems/shuffle-the-array
+public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+
+    }
+}
