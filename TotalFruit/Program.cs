5993bf520f54a8a58c47095a2dbce866dac98ada
staforkin <staforkin.denis@gmail.com>
2023-02-07 20:31:30 +0300

    Setup for Fruit Into Baskets problem

---
+++ TotalFruit/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.TotalFruit(new[] { 1, 2, 1 }));
+
+// https://leetcode.com/problems/fruit-into-baskets
+public class Solution
+{
+    public int TotalFruit(int[] fruits)
+    {
+
+    }
+}
