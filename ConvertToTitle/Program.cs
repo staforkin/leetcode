1ade44bc4272234a5563d870e27ad575087e13e3
staforkin <staforkin.denis@gmail.com>
2023-02-08 20:59:17 +0300

    Setup for Excel Sheet Column Title problem

---
+++ ConvertToTitle/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ConvertToTitle(28));
+
+// https://leetcode.com/problems/excel-sheet-column-title
+public class Solution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+
+    }
+}
