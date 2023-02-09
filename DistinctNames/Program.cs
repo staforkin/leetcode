1dae7300066f7753926bb330cd24825d6c5eb1fc
staforkin <staforkin.denis@gmail.com>
2023-02-09 21:07:30 +0300

    Setup for Naming a Company problem

---
+++ DistinctNames/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.DistinctNames(new[] { "coffee", "donuts", "time", "toffee" }));
+
+// https://leetcode.com/problems/naming-a-company
+public class Solution
+{
+    public long DistinctNames(string[] ideas)
+    {
+
+    }
+}
