8f1bd4f552252699927abd219ecb07f3d7f66af9
staforkin <staforkin.denis@gmail.com>
2023-02-10 18:59:33 +0300

    Setup for As Far from Land as Possible problem

---
+++ MaxDistance/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var arr = new int[][] { new int[] { 1, 0, 1 }, new int[] { 0, 0, 0 }, new int[] { 1, 0, 1 } };
+Console.WriteLine(solution.MaxDistance(arr));
+
+// https://leetcode.com/problems/as-far-from-land-as-possible
+public class Solution
+{
+    public int MaxDistance(int[][] grid)
+    {
+
+    }
+}
