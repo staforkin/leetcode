15bbc6ef4cc26cf43dc1edcd5087a2c9291fa30e
staforkin <staforkin.denis@gmail.com>
2023-02-11 18:26:12 +0300

    Setup for Remove Linked List Elements problem

---
+++ RemoveElements/Program.cs
@@ -0,0 +1,29 @@
+﻿var solution = new Solution();
+var head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(6);
+head.next.next.next = new ListNode(3);
+head.next.next.next.next = new ListNode(4);
+head.next.next.next.next.next = new ListNode(5);
+head.next.next.next.next.next.next = new ListNode(6);
+
+// https://leetcode.com/problems/remove-linked-list-elements
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+
+    }
+}
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
