5497fb955771f36d806d41378088f37987c1fdc1
staforkin <staforkin.denis@gmail.com>
2023-02-12 21:30:01 +0300

    Setup for Minimum Fuel Cost to Report to the Capital problem

---
+++ MinimumFuelCost/Program.cs
@@ -0,0 +1,16 @@
+﻿var solution = new Solution();
+var roads = new int[][]{
+    new int[]{0,1},
+    new int[]{0,2},
+    new int[]{0,3}
+};
+Console.WriteLine(solution.MinimumFuelCost(roads, 5));
+
+// https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital
+public class Solution
+{
+    public long MinimumFuelCost(int[][] roads, int seats)
+    {
+
+    }
+}
