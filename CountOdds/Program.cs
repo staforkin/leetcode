7132c64d27185616b4ae6067e4157b8c9bf628d5
staforkin <staforkin.denis@gmail.com>
2023-02-13 20:23:27 +0300

    Count Odd Numbers in an Interval Range problem

--- CountOdds/Program.cs
+++ CountOdds/Program.cs
@@ -1,11 +1,24 @@
 ﻿var solution = new Solution();
-Console.WriteLine(solution.CountOdds(3, 7));
+Console.WriteLine(solution.CountOdds(4, 8));
 
 // https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
 public class Solution
 {
     public int CountOdds(int low, int high)
     {
+        // 3 4 5 6 7
+        // 3   5   7 = 3
 
+        // 4 5 6 7 8
+        //   5   7   = 2
+
+        if (low % 2 == 0)
+        {
+            return (high - low + 1) / 2;
+        }
+        else
+        {
+            return (high - low) / 2 + 1;
+        }
     }
 }
