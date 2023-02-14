e489e258965f80a9e489593e1fe56c3a5451d336
staforkin <staforkin.denis@gmail.com>
2023-02-14 20:32:14 +0300

    Setup for Isomorphic Strings problem

---
+++ IsIsomorphic/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsIsomorphic("egg", "add"));
+
+// https://leetcode.com/problems/isomorphic-strings
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+
+    }
+}
