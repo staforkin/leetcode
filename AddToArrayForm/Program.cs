fa1226b3aee24f5044b05a09f864a8c6ccac3a27
staforkin <staforkin.denis@gmail.com>
2023-02-15 21:16:21 +0300

    Setup for Add to Array-Form of Integer problem

---
+++ AddToArrayForm/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.AddToArrayForm(new[] { 1, 2, 0, 0, }, 34)));
+
+// https://leetcode.com/problems/add-to-array-form-of-integer
+public class Solution
+{
+    public IList<int> AddToArrayForm(int[] num, int k)
+    {
+        
+    }
+}
