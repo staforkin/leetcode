d4f9987ca4a9b657eb8e47450dc1ff28f83bdb5d
staforkin <staforkin.denis@gmail.com>
2023-02-16 19:48:34 +0300

    Setup for Contains Duplicate II problem

---
+++ ContainsNearbyDuplicate/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ContainsNearbyDuplicate(new[] { 1, 2, 3, 1 }, 3));
+
+// https://leetcode.com/problems/contains-duplicate-ii/
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+
+    }
+}
