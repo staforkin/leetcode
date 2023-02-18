b8cb78e6ddf41a649119513bc508d340a048e74b
staforkin <staforkin.denis@gmail.com>
2023-02-18 17:24:33 +0300

    Summary Ranges problem

--- SummaryRanges/Program.cs
+++ SummaryRanges/Program.cs
@@ -1,5 +1,7 @@
-﻿var solution = new Solution();
-foreach (var item in solution.SummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 }))
+﻿using System.Text;
+
+var solution = new Solution();
+foreach (var item in solution.SummaryRanges(new int[] { 0, 2, 3, 4, 6, 8, 9 }))
 {
     Console.WriteLine(item);
 }
@@ -9,6 +11,30 @@ public class Solution
 {
     public IList<string> SummaryRanges(int[] nums)
     {
+        // 0 1 2 4 5 7
+        // 0 1 1 2 1 2
 
+        // 0 2 3 4 6 8 9
+        // 0 2 1 1 2 2 1
+
+        var res = new List<string>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int start = nums[i];
+            while (i + 1 < nums.Length && nums[i] + 1 == nums[i + 1])
+            {
+                i++;
+            }
+
+            if (start != nums[i])
+            {
+                res.Add($"{start}->{nums[i]}");
+            }
+            else
+            {
+                res.Add(start.ToString());
+            }
+        }
+        return res;
     }
 }
