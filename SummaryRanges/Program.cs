b6c617bee58d908869f2cf2cf49cef59656925d2
staforkin <staforkin.denis@gmail.com>
2023-02-18 16:51:23 +0300

    Setup for Summary Ranges problem

---
+++ SummaryRanges/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.SummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 }))
+{
+    Console.WriteLine(item);
+}
+
+// https://leetcode.com/problems/summary-ranges
+public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+
+    }
+}
