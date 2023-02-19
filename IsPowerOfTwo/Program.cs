5a5762f78d9d734a0a3b28fd001245d24a1a40d1
staforkin <staforkin.denis@gmail.com>
2023-02-19 18:47:27 +0300

    Setup for Power of Two problem

---
+++ IsPowerOfTwo/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsPowerOfTwo(1));
+
+// https://leetcode.com/problems/power-of-two
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+
+    }
+}
