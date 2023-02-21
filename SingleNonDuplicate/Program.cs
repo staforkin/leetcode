fefd8463c9beaa237e9e0387fabfa30e89d49a57
staforkin <staforkin.denis@gmail.com>
2023-02-21 19:39:46 +0300

    Setup for Single Element in a Sorted Array problem

---
+++ SingleNonDuplicate/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SingleNonDuplicate(new[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 }));
+
+// https://leetcode.com/problems/single-element-in-a-sorted-array
+public class Solution
+{
+    public int SingleNonDuplicate(int[] nums)
+    {
+
+    }
+}
