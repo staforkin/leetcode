714a4829026f5be0ff94dccff1d1f6e757327c4c
staforkin <staforkin.denis@gmail.com>
2023-02-22 14:50:16 +0300

    Setup for Capacity To Ship Packages Within D Days problem

---
+++ ShipWithinDays/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ShipWithinDays(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 5));
+
+// https://leetcode.com/problems/capacity-to-ship-packages-within-d-days
+public class Solution
+{
+    public int ShipWithinDays(int[] weights, int days)
+    {
+
+    }
+}
