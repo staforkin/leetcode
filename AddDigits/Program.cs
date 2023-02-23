2f2e2a7ed6a72b7ca1f122c8ae87d123f119f0e0
staforkin <staforkin.denis@gmail.com>
2023-02-23 17:22:40 +0300

    Setup for Add Digits problem

---
+++ AddDigits/Program.cs
@@ -0,0 +1,11 @@
+﻿var soluition = new Solution();
+Console.WriteLine(soluition.AddDigits(38));
+
+// https://leetcode.com/problems/add-digits/
+public class Solution
+{
+    public int AddDigits(int num)
+    {
+
+    }
+}
