85eca3f5317918e0ef9a9a725795e69560a02ef4
staforkin <staforkin.denis@gmail.com>
2023-02-24 12:46:57 +0300

    Setup for Ugly Number problem

---
+++ IsUgly/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsUgly(15));
+
+// https://leetcode.com/problems/ugly-number
+public class Solution
+{
+    public bool IsUgly(int n)
+    {
+
+    }
+}
