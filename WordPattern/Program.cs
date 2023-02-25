b7df26e826d63e76e662a0ea9b7e241bbe70f17c
staforkin <staforkin.denis@gmail.com>
2023-02-25 13:45:22 +0300

    Setup for Word Pattern problem

---
+++ WordPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.WordPattern("abba", "dog cat cat dog"));
+
+// https://leetcode.com/problems/word-pattern
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+
+    }
+}
