07c585869b8b26d34958477ebc69e5ffaf93e783
staforkin <staforkin.denis@gmail.com>
2023-02-26 16:11:10 +0300

    Setup for Nim Game problem

---
+++ CanWinNim/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CanWinNim(4));
+
+// https://leetcode.com/problems/nim-game
+public class Solution
+{
+    public bool CanWinNim(int n)
+    {
+
+    }
+}
