da572d902fd9838f4e09947511973bb9ec750eb0
staforkin <staforkin.denis@gmail.com>
2023-02-27 20:41:43 +0300

    Setup for Integer to Roman problem

---
+++ IntToRoman/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+var solution = new Solution();
+Console.WriteLine(solution.IntToRoman(58));
+Console.WriteLine(solution.IntToRoman(3));
+Console.WriteLine(solution.IntToRoman(1994));
+Console.WriteLine(solution.IntToRoman(1000));
+Console.WriteLine(solution.IntToRoman(2000));
+
+// https://leetcode.com/problems/integer-to-roman
+public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        
+    }
+}
