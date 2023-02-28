550fd5d05531aff17765c9a4402738e4b9360a40
staforkin <staforkin.denis@gmail.com>
2023-02-28 20:15:16 +0300

    Setup for Find Duplicate Subtrees problem

---
+++ FindDuplicateSubtrees/Program.cs
@@ -0,0 +1,30 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.left.left = new TreeNode(4);
+root.right = new TreeNode(3);
+root.right.left = new TreeNode(2);
+root.right.left.left = new TreeNode(4);
+root.right.right = new TreeNode(4);
+
+// https://leetcode.com/problems/find-duplicate-subtrees
+public class Solution
+{
+    public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
