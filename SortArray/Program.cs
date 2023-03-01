32dc0ccf5132d465c6f38af7c28b39edd10409b8
staforkin <staforkin.denis@gmail.com>
2023-03-01 19:17:21 +0300

    Setup for Sort an Array problem

---
+++ SortArray/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SortArray(new[] { 5, 2, 3, 1 })));
+
+// https://leetcode.com/problems/sort-an-array
+public class Solution
+{
+    public int[] SortArray(int[] nums)
+    {
+
+    }
+}
