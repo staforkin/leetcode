7804b59b952aa981c9a3b0624322d1078911cb4c
staforkin <staforkin.denis@gmail.com>
2023-03-02 22:15:03 +0300

    Setup for String Compression problem

---
+++ Compress/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Compress(new []{'a','a','b','b','c','c','c'}));
+
+// https://leetcode.com/problems/string-compression
+public class Solution {
+    public int Compress(char[] chars) {
+        
+    }
+}
