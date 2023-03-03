d63fe0b3ef85e5b7fba3492386a1ef2c41ee03ad
staforkin <staforkin.denis@gmail.com>
2023-03-03 20:59:53 +0300

    Setup for 3Sum Closest problem

---
+++ ThreeSumClosest/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ThreeSumClosest(new[] { -1, 2, 1, -4 }, 2));
+
+// https://leetcode.com/problems/3sum-closest
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+
+    }
+}
