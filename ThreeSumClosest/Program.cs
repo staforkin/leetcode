a27f983c67faf1f29211441a5179d7d2e60a537a
staforkin <staforkin.denis@gmail.com>
2023-03-03 21:11:27 +0300

    3Sum Closest problem

--- ThreeSumClosest/Program.cs
+++ ThreeSumClosest/Program.cs
@@ -6,6 +6,34 @@ public class Solution
 {
     public int ThreeSumClosest(int[] nums, int target)
     {
+        Array.Sort(nums);
+        var closest = 0;
 
+        for (int i = 0, n = nums.Length, minDiff = int.MaxValue; i < n - 2; i++)
+            for (int j = i + 1, k = n - 1; j < k;)
+            {
+                var sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == target)
+                {
+                    return target;
+                }
+                if (sum < target)
+                {
+                    j++;
+                }
+                else
+                {
+                    k--;
+                }
+
+                var diff = Math.Abs(target - sum);
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    closest = sum;
+                }
+            }
+        return closest;
     }
 }
