56994dcb8822ef9a3a0eaccc911dea8477189670
staforkin <staforkin.denis@gmail.com>
2023-03-04 19:20:38 +0300

    Setup for 4Sum problem

---
+++ FourSum/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.FourSum(new[] { 1, 0, -1, 0, -2, 2 }, 0))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/4sum
+public class Solution
+{
+    public IList<IList<int>> FourSum(int[] nums, int target)
+    {
+
+    }
+}
