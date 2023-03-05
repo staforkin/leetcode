16a84b82175ab771d1330f5f0101319e68d169c2
staforkin <staforkin.denis@gmail.com>
2023-03-05 19:42:19 +0300

    Setup for Counting Bits problem

---
+++ CountBits/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.CountBits(2)));
+
+// https://leetcode.com/problems/counting-bits/
+public class Solution
+{
+    public int[] CountBits(int n)
+    {
+
+    }
+}
