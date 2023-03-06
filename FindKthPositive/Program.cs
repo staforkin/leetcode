30c0ec2c8a95505e7d7910d50a0157d453ec33bc
staforkin <staforkin.denis@gmail.com>
2023-03-06 19:45:13 +0300

    Setup for Kth Missing Positive Number problem

---
+++ FindKthPositive/Program.cs
@@ -0,0 +1,10 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindKthPositive(new[] { 2, 3, 4, 7, 11 }, 5));
+
+public class Solution
+{
+    public int FindKthPositive(int[] arr, int k)
+    {
+
+    }
+}
