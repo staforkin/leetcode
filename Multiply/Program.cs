d1ddadff56292e79751a2d7db0a3fc37830a7773
staforkin <staforkin.denis@gmail.com>
2023-03-07 19:35:01 +0300

    Setup for Multiply Strings problem

---
+++ Multiply/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Multiply("2", "3"));
+
+// https://leetcode.com/problems/multiply-strings
+public class Solution
+{
+    public string Multiply(string num1, string num2)
+    {
+
+    }
+}
