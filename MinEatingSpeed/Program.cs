8e051cc09bdd5a060255fba083d505bb3e287750
staforkin <staforkin.denis@gmail.com>
2023-03-08 14:52:17 +0300

    Setup for Koko Eating Bananas problem

---
+++ MinEatingSpeed/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinEatingSpeed(new[] { 3, 6, 7, 11 }, 8));
+
+// https://leetcode.com/problems/koko-eating-bananas
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+
+    }
+}
