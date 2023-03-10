19502c0d8a5c3cf79e42590d535ed01d43b42ed0
staforkin <staforkin.denis@gmail.com>
2023-03-10 22:08:56 +0300

    Setup for Permutations II problem

---
+++ PermuteUnique/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.PermuteUnique(new[] { 1, 2, 3 }))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/permutations-ii
+public class Solution
+{
+    public IList<IList<int>> PermuteUnique(int[] nums)
+    {
+
+    }
+}
