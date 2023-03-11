afecdf7e510580108154c57f793621d8563a075e
staforkin <staforkin.denis@gmail.com>
2023-03-11 18:58:17 +0300

    Setup for Convert Sorted List to Binary Search Tree problem

---
+++ SortedListToBST/Program.cs
@@ -0,0 +1,39 @@
+﻿var solution = new Solution();
+var head = new ListNode(-10);
+head.next = new ListNode(-3);
+head.next.next = new ListNode(0);
+head.next.next.next = new ListNode(5);
+head.next.next.next.next = new ListNode(9);
+
+// https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree
+public class Solution
+{
+    public TreeNode SortedListToBST(ListNode head)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
