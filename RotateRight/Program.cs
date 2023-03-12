5373e2d07f7ac8bae973ce6891858d0289771bc5
staforkin <staforkin.denis@gmail.com>
2023-03-12 17:26:30 +0300

    Setup for Rotate List problem

---
+++ RotateRight/Program.cs
@@ -0,0 +1,32 @@
+﻿var solution = new Solution();
+var head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+var newHead = solution.RotateRight(head, 2);
+while (newHead != null)
+{
+    Console.WriteLine(newHead.val);
+    newHead = newHead.next;
+}
+
+// https://leetcode.com/problems/rotate-list
+public class Solution
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
