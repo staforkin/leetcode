27bcab0648f19370543d086b57e47486771f136a
staforkin <staforkin.denis@gmail.com>
2023-03-13 21:33:50 +0300

    Setup for Unique Paths II problem

---
+++ UniquePathsWithObstacles/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+var grid = new int[][] { new int[] { 0, 0, 0 }, new int[] { 0, 1, 0 }, new int[] { 0, 0, 0 } };
+
+// https://leetcode.com/problems/unique-paths-ii/
+public class Solution
+{
+    public int UniquePathsWithObstacles(int[][] obstacleGrid)
+    {
+
+    }
+}
