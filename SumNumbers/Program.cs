155642bbe8f568ffb9372d1d843130d75135fb2a
staforkin <staforkin.denis@gmail.com>
2023-03-14 20:30:31 +0300

    Setup for Sum Root to Leaf Numbers problem

---
+++ SumNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+Console.WriteLine(solution.SumNumbers(root));
+
+// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+public class Solution {
+    public int SumNumbers(TreeNode root) {
+        
+    }
+}
+
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
