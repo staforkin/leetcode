6294826d6955946abba14b01d14d94de25ba2fcf
staforkin <staforkin.denis@gmail.com>
2023-03-15 20:21:52 +0300

    Setup for Check Completeness of a Binary Tree problem

---
+++ IsCompleteTree/Program.cs
@@ -0,0 +1,30 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+Console.WriteLine(solution.IsCompleteTree(root));
+
+// https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+public class Solution
+{
+    public bool IsCompleteTree(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
