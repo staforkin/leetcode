e2d84eb4d9dd3fb5321464a3746cd3ba5208a2a2
staforkin <staforkin.denis@gmail.com>
2023-03-16 21:11:57 +0300

    Setup for Construct Binary Tree from Inorder and Postorder Traversal problem

---
+++ BuildTree2/Program.cs
@@ -0,0 +1,27 @@
+﻿var solution = new Solution();
+var postorder = new[] { 9, 15, 7, 20, 3 };
+var inOrder = new[] { 9, 3, 15, 20, 7 };
+
+solution.BuildTree(inOrder, postorder);
+
+// https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+public class Solution
+{
+    public TreeNode BuildTree(int[] inorder, int[] postorder)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
