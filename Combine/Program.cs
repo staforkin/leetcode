43c448be21387ef07edf45270730f6074a61a188
staforkin <staforkin.denis@gmail.com>
2023-03-17 19:23:41 +0300

    Setup for Combinations problem

---
+++ Combine/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.Combine(4, 2))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/combinations
+public class Solution
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        
+    }
+}
