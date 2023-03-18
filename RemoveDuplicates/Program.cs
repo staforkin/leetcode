b4564d0cb8b674becc6944e78cfa0e949535ae1f
staforkin <staforkin.denis@gmail.com>
2023-03-18 18:42:01 +0300

    Setup for Remove Duplicates from Sorted Array II problem

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RemoveDuplicates(new int[] { 1, 1, 1, 2, 2, 3 }));
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+
+    }
+}
