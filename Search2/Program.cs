55d8ebe9af52b1f3a5d51f0b98bf71b5c8fe9206
staforkin <staforkin.denis@gmail.com>
2023-03-19 22:31:56 +0300

    Setup for Search in Rotated Sorted Array II problem

---
+++ Search2/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Search(new[] { 2, 5, 6, 0, 0, 1, 2 }, 0));
+
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii
+public class Solution
+{
+    public bool Search(int[] nums, int target)
+    {
+
+    }
+}
