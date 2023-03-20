5b2ea7e4ca09c5ca31283472a027dff293be8673
staforkin <staforkin.denis@gmail.com>
2023-03-20 20:44:56 +0300

    Setup for Can Place Flowers problem

---
+++ CanPlaceFlowers/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CanPlaceFlowers(new[] { 1, 0, 0, 0, 1 }, 1));
+
+// https://leetcode.com/problems/can-place-flowers/
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+
+    }
+}
