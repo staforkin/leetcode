a23b5082ab78dbe39934fe352314798bac96532a
staforkin <staforkin.denis@gmail.com>
2023-03-21 19:37:32 +0300

    Setup for Number of Zero-Filled Subarrays problem

---
+++ ZeroFilledSubarray/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ZeroFilledSubarray(new[] { 1, 3, 0, 0, 2, 0, 0, 4 }));
+
+// https://leetcode.com/problems/number-of-zero-filled-subarrays
+public class Solution
+{
+    public long ZeroFilledSubarray(int[] nums)
+    {
+        
+    }
+}
