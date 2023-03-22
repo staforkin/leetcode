44a72b72695635b755779235bcc3c380e64592cb
staforkin <staforkin.denis@gmail.com>
2023-03-22 20:32:56 +0300

    Setup for Minimum Score of a Path Between Two Cities problem

---
+++ MinScore/Program.cs
@@ -0,0 +1,16 @@
+﻿var solution = new Solution();
+var roads = new int[][] {
+    new int[] { 1, 2, 9 },
+    new int[] { 2, 3, 6 },
+    new int[] { 2, 4, 5 },
+    new int[] { 1, 4, 7 } };
+Console.WriteLine(solution.MinScore(4, roads));
+
+// https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities
+public class Solution
+{
+    public int MinScore(int n, int[][] roads)
+    {
+
+    }
+}
