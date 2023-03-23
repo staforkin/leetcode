95f0b361f8791f67a87af015acc3cc3864adf01f
staforkin <staforkin.denis@gmail.com>
2023-03-23 20:34:06 +0300

    Setup for Number of Operations to Make Network Connected problem

---
+++ MakeConnected/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var connections = new int[][] { new int[] { 0, 1 }, new int[] { 0, 2 }, new int[] { 1, 2 } };
+Console.WriteLine(solution.MakeConnected(4,connections));
+
+// https://leetcode.com/problems/number-of-operations-to-make-network-connected
+public class Solution
+{
+    public int MakeConnected(int n, int[][] connections)
+    {
+
+    }
+}
