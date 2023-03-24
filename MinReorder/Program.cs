024a96f8f7d2986ddb592d4f2a866efcdc412d65
staforkin <staforkin.denis@gmail.com>
2023-03-24 18:25:52 +0300

    Setup for Reorder Routes to Make All Paths Lead to the City Zero problem

---
+++ MinReorder/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var connections = new int[][] { new int[] { 0, 1 }, new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 4, 0 }, new int[] { 4, 5 } };
+Console.WriteLine(solution.MinReorder(6, connections));
+
+// https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero
+public class Solution
+{
+    public int MinReorder(int n, int[][] connections)
+    {
+
+    }
+}
