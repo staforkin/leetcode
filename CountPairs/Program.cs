643774bdaee2d5da56cf62122fee864a94eae05f
staforkin <staforkin.denis@gmail.com>
2023-03-25 18:40:11 +0300

    Setup for Count Unreachable Pairs of Nodes in an Undirected Graph problem

---
+++ CountPairs/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+var edges = new int[][] {
+    new int[] { 0, 1 },
+    new int[] { 0, 2 },
+    new int[] { 1, 2 } };
+
+// https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph
+public class Solution
+{
+    public long CountPairs(int n, int[][] edges)
+    {
+
+    }
+}
