a85fdd076c2cbf9726b122b8d7875d47250dc598
staforkin <staforkin.denis@gmail.com>
2023-03-26 22:05:58 +0300

    Setup for Longest Cycle in a Graph problem

---
+++ LongestCycle/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LongestCycle(new[] { 3, 3, 4, 2, 3 }));
+
+// https://leetcode.com/problems/longest-cycle-in-a-graph
+public class Solution
+{
+    public int LongestCycle(int[] edges)
+    {
+
+    }
+}
