e0f3f52928d7f52b741a133cc45c12ba074282e9
staforkin <staforkin.denis@gmail.com>
2023-03-27 21:10:37 +0300

    Setup for Intersection of Two Arrays problem

---
+++ Intersection/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.Intersection(new[] { 1, 2, 2, 1 }, new[] { 2, 2 })));
+
+// https://leetcode.com/problems/intersection-of-two-arrays
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+
+    }
+}
