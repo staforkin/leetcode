af47bd77bbad1badd34c3516403fbcdd41fdb27b
staforkin <staforkin.denis@gmail.com>
2023-03-28 20:30:30 +0300

    Setup for Minimum Cost For Tickets problem

---
+++ MincostTickets/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MincostTickets(new[] { 1, 4, 6, 7, 8, 20 }, new[] { 2, 7, 15 }));
+
+// https://leetcode.com/problems/minimum-cost-for-tickets
+public class Solution
+{
+    public int MincostTickets(int[] days, int[] costs)
+    {
+
+    }
+}
