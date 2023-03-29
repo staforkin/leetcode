543c3659e3f74fbeee7f24ca607f43bf31d237fa
staforkin <staforkin.denis@gmail.com>
2023-03-29 20:31:45 +0300

    Setup for Reducing Dishes problem

---
+++ MaxSatisfaction/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxSatisfaction(new[] { -1, -8, 0, 5, -9 }));
+
+// https://leetcode.com/problems/reducing-dishes
+public class Solution
+{
+    public int MaxSatisfaction(int[] satisfaction)
+    {
+
+    }
+}
