2cb77d5c183a16bcceb4c069f980ec7d7ea4566c
staforkin <staforkin.denis@gmail.com>
2023-03-30 20:40:11 +0300

    Setup for Scramble String problem

---
+++ IsScramble/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsScramble("great", "rgeat"));
+
+// https://leetcode.com/problems/scramble-string
+public class Solution
+{
+    public bool IsScramble(string s1, string s2)
+    {
+
+    }
+}
