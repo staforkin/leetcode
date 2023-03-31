bf632b7c0b235e4697cf13509d63944613408cad
staforkin <staforkin.denis@gmail.com>
2023-03-31 20:30:51 +0300

    Setup for Number of Ways of Cutting a Pizza problem

---
+++ Ways/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Ways(new[] { "A..", "AAA", "..." }, 3));
+
+// https://leetcode.com/problems/number-of-ways-of-cutting-a-pizza
+public class Solution
+{
+    public int Ways(string[] pizza, int k)
+    {
+        
+    }
+}
