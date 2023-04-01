8d672cb4c8b0d66c2b55c7a59f861bb58653578b
staforkin <staforkin.denis@gmail.com>
2023-04-01 19:38:29 +0300

    Setup for Binary Search problem

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Search(new[] { -1, 0, 3, 5, 9, 12 }, 9));
+
+// https://leetcode.com/problems/binary-search
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+
+    }
+}
