670c9e2c1bca310466d91cb7a7e9c4142af7e714
staforkin <staforkin.denis@gmail.com>
2023-04-02 10:07:29 +0300

    Setup for Successful Pairs of Spells and Potions problem

---
+++ SuccessfulPairs/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SuccessfulPairs(new[] { 5, 1, 3 }, new[] { 1, 2, 3, 4, 5 }, 7)));
+
+// https://leetcode.com/problems/successful-pairs-of-spells-and-potions
+public class Solution
+{
+    public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+    {
+
+    }
+}
