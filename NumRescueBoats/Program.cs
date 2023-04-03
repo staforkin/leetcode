f924e20055346bf2c0d08d17798a0b953eca4fb1
staforkin <staforkin.denis@gmail.com>
2023-04-03 20:11:34 +0300

    Setup for Boats to Save People problem

---
+++ NumRescueBoats/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumRescueBoats(new[] { 1, 2 }, 3));
+
+// https://leetcode.com/problems/boats-to-save-people
+public class Solution
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+
+    }
+}
