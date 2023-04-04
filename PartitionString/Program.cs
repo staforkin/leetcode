8836e9a6250b2e6e37c25f69aae45766ddc316f7
staforkin <staforkin.denis@gmail.com>
2023-04-04 21:06:20 +0300

    Setup for Optimal Partition of String problem

---
+++ PartitionString/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.PartitionString("abacaba"));
+
+// https://leetcode.com/problems/optimal-partition-of-string
+public class Solution
+{
+    public int PartitionString(string s)
+    {
+
+    }
+}
