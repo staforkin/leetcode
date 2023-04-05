10c85066dcecfb9328cae11b4d4cf28abf33723c
staforkin <staforkin.denis@gmail.com>
2023-04-05 18:24:15 +0300

    Minimize Maximum of Array problem

---
+++ MinimizeArrayValue/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinimizeArrayValue(new[] { 3, 7, 1, 6 }));
+
+// https://leetcode.com/problems/minimize-maximum-of-array/
+public class Solution
+{
+    public int MinimizeArrayValue(int[] nums)
+    {
+
+    }
+}
