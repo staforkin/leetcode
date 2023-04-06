a6f9312aa707a8710a4431fc5daa1734ea712279
staforkin <staforkin.denis@gmail.com>
2023-04-06 19:18:11 +0300

    Setup for Number of Closed Islands problem

---
+++ ClosedIsland/Program.cs
@@ -0,0 +1,18 @@
+﻿var solution = new Solution();
+var grid = new int[][]{
+    new int[]{1,1,1,1,1,1,1,0},
+    new int[]{1,0,0,0,0,1,1,0},
+    new int[]{1,0,1,0,1,1,1,0},
+    new int[]{1,0,0,0,0,1,0,1},
+    new int[]{1,1,1,1,1,1,1,0},
+};
+Console.WriteLine(solution.ClosedIsland(grid));
+
+// https://leetcode.com/problems/number-of-closed-islands
+public class Solution
+{
+    public int ClosedIsland(int[][] grid)
+    {
+
+    }
+}
