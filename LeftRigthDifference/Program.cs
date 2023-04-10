27c86d2f7a5c0f186cc400451140b443a4103ef5
staforkin <staforkin.denis@gmail.com>
2023-04-10 18:42:22 +0300

    Left and Right Sum Differences problem

---
+++ LeftRigthDifference/Program.cs
@@ -0,0 +1,24 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.LeftRigthDifference(new[] { 10, 4, 8, 3 })));
+
+// https://leetcode.com/problems/left-and-right-sum-differences
+public class Solution
+{
+    public int[] LeftRigthDifference(int[] nums)
+    {
+        int n = nums.Length;
+        int[] ans = new int[n];
+        int[] a = new int[n + 1];
+        a[0] = 0;
+        for (int i = 0; i < n; i++)
+        {
+            a[i + 1] = a[i] + nums[i];
+        }
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            ans[i - 1] = Math.Abs(a[n] - a[i] - a[i - 1]);
+        }
+        return ans;
+    }
+}
