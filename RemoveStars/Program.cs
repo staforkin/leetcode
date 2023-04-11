d08fc088ed6f119f3d2898428bee6074aa40a18b
staforkin <staforkin.denis@gmail.com>
2023-04-11 22:12:39 +0300

    Setup for Removing Stars From a String problem

---
+++ RemoveStars/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RemoveStars("leet**cod*e"));
+
+// https://leetcode.com/problems/removing-stars-from-a-string
+public class Solution
+{
+    public string RemoveStars(string s)
+    {
+
+    }
+}
