1445bdaf585e3399ce853dd24ad32a9d8e31f5d9
staforkin <staforkin.denis@gmail.com>
2023-04-12 19:15:04 +0300

    Setup for Simplify Path problem

---
+++ SimplifyPath/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SimplifyPath("/home/"));
+
+// 
+public class Solution
+{
+    public string SimplifyPath(string path)
+    {
+        
+    }
+}
