03f6feea9b154fb6158a4d74ce1f86617ba30d2b
staforkin <staforkin.denis@gmail.com>
2023-04-13 19:38:14 +0300

    Setup for Validate Stack Sequences problem

---
+++ ValidateStackSequences/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ValidateStackSequences(new[] { 1, 2, 3, 4, 5 }, new[] { 4, 5, 3, 2, 1 }));
+
+// https://leetcode.com/problems/validate-stack-sequences/
+public class Solution
+{
+    public bool ValidateStackSequences(int[] pushed, int[] popped)
+    {
+
+    }
+}
