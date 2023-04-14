54b28995d41e521df554a11001e970c749287cf8
staforkin <staforkin.denis@gmail.com>
2023-04-15 00:00:03 +0300

    Longest Palindromic Subsequence

---
+++ LongestPalindromeSubseq/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class Solution
+{
+    public int LongestPalindromeSubseq(string s)
+    {
+        int[][] dp = new int[s.Length][];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            dp[i] = new int[s.Length];
+        }
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < s.Length; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                }
+                else
+                {
+                    dp[i][j] = Math.Max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.Length - 1];
+    }
+}
