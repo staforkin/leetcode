97ee72b6e92aa9537673ce8331faae56908d27d0
staforkin <staforkin.denis@gmail.com>
2023-04-15 18:19:13 +0300

    Setup for Maximum Value of K Coins From Piles problem

---
+++ MaxValueOfCoins/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxValueOfCoins(new List<List<int>>(new[] { 1, 100, 3 }, new[] { 7, 8, 9 })));
+
+// https://leetcode.com/problems/maximum-value-of-k-coins-from-piles
+public class Solution
+{
+    public int MaxValueOfCoins(IList<IList<int>> piles, int k)
+    {
+
+    }
+}
