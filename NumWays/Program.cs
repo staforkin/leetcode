ef50835fb60fc68cdd26ea49702edaaa86fcc0b6
staforkin <staforkin.denis@gmail.com>
2023-04-16 17:29:19 +0300

    Setup for Number of Ways to Form a Target String Given a Dictionary problem

---
+++ NumWays/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumWays(new[] { "acca", "bbbb", "caca" }, "aba"));
+
+// https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/
+public class Solution
+{
+    public int NumWays(string[] words, string target)
+    {
+
+    }
+}
