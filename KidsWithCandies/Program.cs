d6296dce9b22b7c24ed6d8ffe6a4309908d77872
staforkin <staforkin.denis@gmail.com>
2023-04-17 18:23:03 +0300

    Setup for Kids With the Greatest Number of Candies problem

---
+++ KidsWithCandies/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.KidsWithCandies(new[] { 2, 3, 5, 1, 3 }, 3)));
+
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+
+    }
+}
