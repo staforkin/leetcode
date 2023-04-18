564965205bebc17913d7689e33ab8061a085b159
staforkin <staforkin.denis@gmail.com>
2023-04-18 19:47:11 +0300

    Setup for Merge Strings Alternately problem

---
+++ MergeAlternately/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MergeAlternately("abc", "pqr"));
+
+// https://leetcode.com/problems/merge-strings-alternately
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+
+    }
+}
