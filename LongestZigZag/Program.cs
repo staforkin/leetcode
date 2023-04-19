10d0390db954a2336ca99a1fc92b772d9520b860
staforkin <staforkin.denis@gmail.com>
2023-04-19 20:56:25 +0300

    Setup for Longest ZigZag Path in a Binary Tree problem

---
+++ LongestZigZag/Program.cs
@@ -0,0 +1,29 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.right = new TreeNode(1);
+root.right.right = new TreeNode(1);
+root.right.right.left = new TreeNode(1);
+root.right.right.left.right = new TreeNode(1);
+Console.WriteLine(solution.LongestZigZag(root));
+
+public class Solution
+{
+    public int LongestZigZag(TreeNode root)
+    {
+
+    }
+}
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
