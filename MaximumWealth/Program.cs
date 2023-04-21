424ace2dfa6570d96d5b4818f46b0360599fa33a
staforkin <staforkin.denis@gmail.com>
2023-04-21 20:42:39 +0300

    Setup for Richest Customer Wealth problem

---
+++ MaximumWealth/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var accounts = new int[][] { new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 } };
+Console.WriteLine(solution.MaximumWealth(accounts));
+
+// https://leetcode.com/problems/richest-customer-wealth
+public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+
+    }
+}
