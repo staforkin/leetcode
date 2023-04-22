5b4f86061de7776289c7fb9ec0106f7cdd1208ed
staforkin <staforkin.denis@gmail.com>
2023-04-22 21:42:46 +0300

    Setup for Minimum Time Visiting All Points problem

---
+++ MinTimeToVisitAllPoints/Program.cs
@@ -0,0 +1,12 @@
+var solution = new Solution();
+var points = new int[][] { new int[] { 1, 1 }, new int[] { 3, 4 }, new int[] { -1, 0 } };
+Console.WriteLine(solution.MinTimeToVisitAllPoints(points));
+
+// https://leetcode.com/problems/minimum-time-visiting-all-points
+public class Solution
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        return 0;
+    }
+}
