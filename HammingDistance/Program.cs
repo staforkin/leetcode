0d71228c948431d266fc018e4045047eb36ac5ff
staforkin <staforkin.denis@gmail.com>
2023-04-23 18:36:59 +0300

    Setup for Hamming Distance problem

---
+++ HammingDistance/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.HammingDistance(1, 4));
+
+// https://leetcode.com/problems/hamming-distance
+public class Solution
+{
+    public int HammingDistance(int x, int y)
+    {
+
+    }
+}
