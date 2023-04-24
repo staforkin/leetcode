60c9034ad794ecb8fd67ce92e5b868452228a1eb
staforkin <staforkin.denis@gmail.com>
2023-04-24 18:46:38 +0300

    Setup for Last Stone Weight problem

---
+++ LastStoneWeight/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LastStoneWeight());
+
+// https://leetcode.com/problems/last-stone-weight
+public class Solution
+{
+    public int LastStoneWeight(int[] stones)
+    {
+
+    }
+}
