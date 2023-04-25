e2267b2cf5b620105b13ce51d28859981d65b88a
staforkin <staforkin.denis@gmail.com>
2023-04-25 20:32:12 +0300

    Setup for Max Consecutive Ones problem

---
+++ FindMaxConsecutiveOnes/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindMaxConsecutiveOnes(new int[] { 1, 0, 1, 1, 1 }));
+
+// https://leetcode.com/problems/max-consecutive-ones
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+
+    }
+}
