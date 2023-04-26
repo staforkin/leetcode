6736c76f8a3ea9d88a185a931054dcd41514ffa4
staforkin <staforkin.denis@gmail.com>
2023-04-26 21:04:45 +0300

    Setup for Find All Numbers Disappeared in an Array problem

---
+++ FindDisappearedNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FindDisappearedNumbers(new[] { 4, 3, 2, 7, 8, 2, 3, 1 })));
+
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+
+    }
+}
