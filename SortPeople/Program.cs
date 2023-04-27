b9f7bc851197e13dacc204374f492e03a94a7f8d
staforkin <staforkin.denis@gmail.com>
2023-04-27 20:08:54 +0300

    Setup for Sort the People problem

---
+++ SortPeople/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SortPeople(new[] { "Mary", "John", "Emma" }, new[] { 180, 165, 170 })));
+
+// https://leetcode.com/problems/sort-the-people
+public class Solution
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+
+    }
+}
