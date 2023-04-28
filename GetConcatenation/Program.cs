324fae3b29e5375761ab6100001ae207d4bb34db
staforkin <staforkin.denis@gmail.com>
2023-04-28 18:50:22 +0300

    Setup for Concatenation of Array problem

---
+++ GetConcatenation/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.GetConcatenation(new int[] { 1, 2, 1 })));
+
+// https://leetcode.com/problems/concatenation-of-array
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+
+    }
+}
