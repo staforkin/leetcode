14e629c0b8ec941de0f584c27221b13cf71924be
staforkin <staforkin.denis@gmail.com>
2023-04-29 14:59:34 +0300

    Setup for Check if Number Has Equal Digit Count and Digit Value problem

---
+++ DigitCount/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.DigitCount("1210"));
+
+// https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value
+public class Solution
+{
+    public bool DigitCount(string num)
+    {
+
+    }
+}
