8bd3a0b09b3ea9985547f722094ae886c78c52eb
staforkin <staforkin.denis@gmail.com>
2023-04-30 19:29:47 +0300

    Setup for Replace Elements with Greatest Element on Right Side problem

---
+++ ReplaceElements/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.ReplaceElements(new[] { 17, 18, 5, 4, 6, 1 })));
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+
+    }
+}
