eccc2e8e4a95184a80b3663461939e2440dc8145
staforkin <staforkin.denis@gmail.com>
2023-05-01 20:34:09 +0300

    Setup for Average Salary Excluding the Minimum and Maximum Salary problem

---
+++ Average/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Average(new[] { 4000, 3000, 1000, 2000 }));
+
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary
+public class Solution
+{
+    public double Average(int[] salary)
+    {
+
+    }
+}
