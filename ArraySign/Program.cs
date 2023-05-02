cc80d2079f30ee1212cd2aa108c4079a7ecb23aa
staforkin <staforkin.denis@gmail.com>
2023-05-02 15:41:17 +0300

    Setup for Sign of the Product of an Array problem

---
+++ ArraySign/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ArraySign(new[] { -1, -2, -3, -4, 3, 2, 1 }));
+
+// https://leetcode.com/problems/sign-of-the-product-of-an-array
+public class Solution
+{
+    public int ArraySign(int[] nums)
+    {
+
+    }
+}
