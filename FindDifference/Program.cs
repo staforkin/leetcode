249632ccb8a0befcfa4d80c9045e66dd708db25f
staforkin <staforkin.denis@gmail.com>
2023-05-03 20:11:33 +0300

    Setup for Find the Difference of Two Arrays problem

---
+++ FindDifference/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.FindDifference(new[] { 1, 2, 3 }, new[] { 2, 4, 6 }))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/find-the-difference-of-two-arrays
+public class Solution
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+
+    }
+}
