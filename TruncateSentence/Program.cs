de7c13cb577b7dcf33ec86e89da94cabee1caf1c
staforkin <staforkin.denis@gmail.com>
2023-05-04 22:04:44 +0300

    Setup for Truncate Sentence problem

---
+++ TruncateSentence/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.TruncateSentence("Hello how are you Contestant", 4));
+
+// https://leetcode.com/problems/truncate-sentence
+public class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+
+    }
+}
