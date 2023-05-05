86d68196d8066a796d5ec9e0f6cc9dfd89aa4956
staforkin <staforkin.denis@gmail.com>
2023-05-05 12:29:42 +0300

    Setup for Maximum Number of Vowels in a Substring of Given Length

---
+++ MaxVowels/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxVowels("abciiidef", 3));
+
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length
+public class Solution
+{
+    public int MaxVowels(string s, int k)
+    {
+
+    }
+}
