0bd2c331cda2fa0560784862b98595b388813ad3
staforkin <staforkin.denis@gmail.com>
2023-05-06 21:10:43 +0300

    Setup for Number of Subsequences That Satisfy the Given Sum Condition problem

---
+++ TruncateSentence/NumSubseq/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumSubseq(new[] { 3, 5, 6, 7 }, 9));
+
+// https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition
+public class Solution
+{
+    public int NumSubseq(int[] nums, int target)
+    {
+
+    }
+}
