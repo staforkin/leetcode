ead7cc031f6b8e03a2ea33304b9abc7e1628423a
staforkin <staforkin.denis@gmail.com>
2023-05-07 18:13:02 +0300

    Setup for Count Items Matching a Rule problem

---
+++ TruncateSentence/CountMatches/Program.cs
@@ -0,0 +1,15 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CountMatches(new[]{
+new List<string>(){"phone","blue","pixel"},
+new List<string>(){"computer","silver","lenovo"},
+new List<string>(){"phone","gold","iphone"}},
+"color", "silver"));
+
+// https://leetcode.com/problems/count-items-matching-a-rule
+public class Solution
+{
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+
+    }
+}
