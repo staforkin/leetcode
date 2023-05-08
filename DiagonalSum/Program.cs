c05557c05a4cd8a3ce0fdd06d5429a67de83afc5
staforkin <staforkin.denis@gmail.com>
2023-05-08 17:22:44 +0300

    Setup for Matrix Diagonal Sum problem

---
+++ DiagonalSum/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var matrix = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } };
+Console.WriteLine(solution.DiagonalSum(matrix));
+
+// https://leetcode.com/problems/matrix-diagonal-sum
+public class Solution
+{
+    public int DiagonalSum(int[][] mat)
+    {
+
+    }
+}
