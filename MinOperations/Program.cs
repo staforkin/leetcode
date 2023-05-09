e1424b58b9545c245b8c2359392b62f2d648358b
staforkin <staforkin.denis@gmail.com>
2023-05-09 19:08:28 +0300

    Setup for Minimum Operations to Make the Array Increasing problem

---
+++ MinOperations/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinOperations(new[] { 1, 1, 1 }));
+
+// https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing
+public class Solution
+{
+    public int MinOperations(int[] nums)
+    {
+
+    }
+}
