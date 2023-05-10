31afbe34dd160b184d21c09f1bdfbfe0effd921c
staforkin <staforkin.denis@gmail.com>
2023-05-10 20:45:22 +0300

    Setup for Spiral Matrix II problem

---
+++ GenerateMatrix/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.GenerateMatrix(3))
+{
+    Console.WriteLine(string.Join("\t", item));
+}
+
+// https://leetcode.com/problems/spiral-matrix-ii
+public class Solution
+{
+    public int[][] GenerateMatrix(int n)
+    {
+
+    }
+}
