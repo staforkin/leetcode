e5933d429394ec437e32f50f292b5603dc2d0d2d
staforkin <staforkin.denis@gmail.com>
2023-05-11 21:40:04 +0300

    Setup for Uncrossed Lines problem

---
+++ MaxUncrossedLines/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxUncrossedLines(new[] { 1, 4, 2 }, new[] { 1, 2, 4 }));
+
+// https://leetcode.com/problems/uncrossed-lines/
+public class Solution
+{
+    public int MaxUncrossedLines(int[] nums1, int[] nums2)
+    {
+
+    }
+}
