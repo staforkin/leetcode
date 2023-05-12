846fa1741f69a9d396e51ef914e3b08816dd80f3
staforkin <staforkin.denis@gmail.com>
2023-05-12 22:30:49 +0300

    Setup for Replace All Digits with Characters problem

---
+++ ReplaceDigits/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ReplaceDigits("a1c1e1"));
+
+// https://leetcode.com/problems/replace-all-digits-with-characters/
+public class Solution
+{
+    public string ReplaceDigits(string s)
+    {
+
+    }
+}
