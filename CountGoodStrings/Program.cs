ee6a47a2f32f7a6fc29fdc4b74f375eedded3f3f
staforkin <staforkin.denis@gmail.com>
2023-05-13 22:19:17 +0300

    Setup for Count Ways To Build Good Strings problem

---
+++ CountGoodStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CountGoodStrings(3, 3, 1, 1));
+
+// https://leetcode.com/problems/count-ways-to-build-good-strings
+public class Solution
+{
+    public int CountGoodStrings(int low, int high, int zero, int one)
+    {
+
+    }
+}
