5529a2872310d45001e290c9f287eee37c6ec282
staforkin <staforkin.denis@gmail.com>
2023-05-14 17:35:11 +0300

    Setup for Count Number of Pairs With Absolute Difference K problem

---
+++ CountKDifference/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CountKDifference(new[] { 1, 2, 2, 1 }, 1));
+
+// https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k
+public class Solution
+{
+    public int CountKDifference(int[] nums, int k)
+    {
+
+    }
+}
