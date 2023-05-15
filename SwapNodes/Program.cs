8bb2c2aecaa05607f7da752464da7c2f153fbc4c
staforkin <staforkin.denis@gmail.com>
2023-05-15 20:49:14 +0300

    Setup for Swapping Nodes in a Linked List problem

---
+++ SwapNodes/Program.cs
@@ -0,0 +1,24 @@
+﻿var solution = new Solution();
+var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+var newHead = solution.SwapNodes(head, 2);
+
+// https://leetcode.com/problems/swapping-nodes-in-a-linked-list
+public class Solution
+{
+    public ListNode SwapNodes(ListNode head, int k)
+    {
+
+    }
+}
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
