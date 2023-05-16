efa1f935c5a565305f3b06f531ca8c7b9a8fd257
staforkin <staforkin.denis@gmail.com>
2023-05-16 19:50:13 +0300

    Setup for Sorting the Sentence problem

---
+++ SortSentence/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SortSentence("is2 sentence4 This1 a3"));
+
+// https://leetcode.com/problems/sorting-the-sentence
+public class Solution
+{
+    public string SortSentence(string s)
+    {
+
+    }
+}
