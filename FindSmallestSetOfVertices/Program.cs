f386a8dfd90559edff2edde8a06013bd35de6367
staforkin <staforkin.denis@gmail.com>
2023-05-18 18:30:53 +0300

    Setup for Minimum Number of Vertices to Reach All Nodes problem

---
+++ FindSmallestSetOfVertices/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FindSmallestSetOfVertices(6, new[] { new[] { 0, 1 }, new[] { 0, 2 }, new[] { 2, 5 }, new[] { 3, 4 }, new[] { 4, 2 } })));
+
+// https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes
+public class Solution
+{
+    public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+    {
+
+    }
+}
