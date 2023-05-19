b686e1e28d6f267ec2d7ff5e6f0eca95c07bcc13
staforkin <staforkin.denis@gmail.com>
2023-05-19 18:53:05 +0300

    Setup for Is Graph Bipartite? problem

---
+++ IsBipartite/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var graph = new int[][] { new int[] { 1, 2, 3 }, new int[] { 0, 2 }, new int[] { 0, 1, 3 }, new int[] { 0, 2 } };
+Console.WriteLine(solution.IsBipartite(graph));
+
+// https://leetcode.com/problems/is-graph-bipartite
+public class Solution
+{
+    public bool IsBipartite(int[][] graph)
+    {
+
+    }
+}
