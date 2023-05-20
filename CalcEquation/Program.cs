418de4045ed6e2efb68b0fbef8205dabe8f4a5a0
staforkin <staforkin.denis@gmail.com>
2023-05-20 12:22:03 +0300

    Setup for Evaluate Division problem

---
+++ CalcEquation/Program.cs
@@ -0,0 +1,17 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join("", solution.CalcEquation(new[] { new[] { "a", "b" }, new[] { "b", "c" } }, new[] { 2.0, 3.0 }, new[]{
+    new []{"a","c"},
+    new []{"b","a"},
+    new []{"a","e"},
+    new []{"a","a"},
+    new []{"x","x"}
+})));
+
+// https://leetcode.com/problems/evaluate-division
+public class Solution
+{
+    public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+    {
+
+    }
+}
