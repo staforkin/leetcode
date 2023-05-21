59e1c49852f622f0b5fd7fa761f51d7384a162c1
staforkin <staforkin.denis@gmail.com>
2023-05-21 13:08:39 +0300

    Setup for Shortest Bridge problem

---
+++ ShortestBridge/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var grid = new int[][] { new[] { 0, 1 }, new[] { 1, 0 } };
+Console.WriteLine(solution.ShortestBridge(grid));
+
+// https://leetcode.com/problems/shortest-bridge
+public class Solution
+{
+    public int ShortestBridge(int[][] grid)
+    {
+
+    }
+}
