0f89952b15208e276a6b7d07ffc6be30491fb72e
staforkin <staforkin.denis@gmail.com>
2023-05-23 21:00:41 +0300

    Setup for Kth Largest Element in a Stream problem

---
+++ KthLargest/Program.cs
@@ -0,0 +1,18 @@
+﻿var solution = new KthLargest(3, new []{4,5,8,2});
+solution.Add(3);
+solution.Add(5);
+solution.Add(10);
+solution.Add(9);
+solution.Add(4);
+
+// https://leetcode.com/problems/kth-largest-element-in-a-stream
+public class KthLargest {
+
+    public KthLargest(int k, int[] nums) {
+        
+    }
+    
+    public int Add(int val) {
+        
+    }
+}
