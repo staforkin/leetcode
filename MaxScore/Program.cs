ec1a98cbf0363e25cd18614d8b8821c0348fb6dc
staforkin <staforkin.denis@gmail.com>
2023-05-24 20:50:49 +0300

    Setup for Maximum Subsequence Score problem

---
+++ MaxScore/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxScore(new[] { 1, 3, 3, 2 }, new[] { 2, 1, 3, 4 }, 3));
+
+// https://leetcode.com/problems/maximum-subsequence-score
+public class Solution
+{
+    public long MaxScore(int[] nums1, int[] nums2, int k)
+    {
+
+    }
+}
