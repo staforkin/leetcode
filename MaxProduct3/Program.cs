0187d0e1d6ef35159648f907057ebdd6115fb0b5
staforkin <staforkin.denis@gmail.com>
2023-05-25 20:26:46 +0300

    Setup for Maximum Product of Two Elements in an Array problem

---
+++ MaxProduct3/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxProduct(new[] { 3, 4, 5, 2 }));
+
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+public class Solution
+{
+    public int MaxProduct(int[] nums)
+    {
+
+    }
+}
