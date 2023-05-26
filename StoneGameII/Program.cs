055647c44d027e7a91a34c0218ebef709061a820
staforkin <staforkin.denis@gmail.com>
2023-05-26 21:32:41 +0300

    Setup for Stone Game II problem

---
+++ StoneGameII/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.StoneGameII(new[] { 2, 7, 9, 4, 4 }));
+
+// https://leetcode.com/problems/stone-game-ii/
+public class Solution
+{
+    public int StoneGameII(int[] piles)
+    {
+
+    }
+}
