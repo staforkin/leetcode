81e8ab284c0e04748d38c51567dfae83dc702f87
staforkin <staforkin.denis@gmail.com>
2023-05-27 12:25:24 +0300

    Setup for Goal Parser Interpretation problem

---
+++ Interpret/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Interpret("G()(al)"));
+
+// https://leetcode.com/problems/goal-parser-interpretation
+public class Solution
+{
+    public string Interpret(string command)
+    {
+
+    }
+}
