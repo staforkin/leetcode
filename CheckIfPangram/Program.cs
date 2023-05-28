261c6c7e02c56772514cfafe5eaa8c03a6f4dd57
staforkin <staforkin.denis@gmail.com>
2023-05-28 15:23:17 +0300

    Setup for Check if the Sentence Is Pangram problem

---
+++ CheckIfPangram/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CheckIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram
+public class Solution
+{
+    public bool CheckIfPangram(string sentence)
+    {
+
+    }
+}
