d4ecc741eea119f767f6450b6d8cbb4adb8f7e72
staforkin <staforkin.denis@gmail.com>
2023-05-29 21:08:06 +0300

    Setup for Maximum Nesting Depth of the Parentheses problem

---
+++ MaxDepth2/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaxDepth("(1+(2*3)+((8)/4))+1"));
+
+// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+public class Solution
+{
+    public int MaxDepth(string s)
+    {
+
+    }
+}
