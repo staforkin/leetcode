910fd6793eabb40f554ac35ef6d7dbdcc9d9ff96
staforkin <staforkin.denis@gmail.com>
2023-05-30 19:48:16 +0300

    Setup for Running Sum of 1d Array problem

---
+++ RunningSum/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.RunningSum(new[] { 1, 2, 3, 4 })));
+
+// https://leetcode.com/problems/running-sum-of-1d-array
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+
+    }
+}
