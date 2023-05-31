8d62c673e198aa6c1d9c0c17cc64316f4723904e
staforkin <staforkin.denis@gmail.com>
2023-05-31 21:56:56 +0300

    Sum of All Odd Length Subarrays problem

--- README.md
+++ README.md
@@ -254,3 +254,4 @@ List of problems solved:
 - [Check if the Sentence Is Pangram](https://leetcode.com/problems/check-if-the-sentence-is-pangram)
 - [Maximum Nesting Depth of the Parentheses](https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses)
 - [Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array)
+- [Sum of All Odd Length Subarrays](https://leetcode.com/problems/sum-of-all-odd-length-subarrays)
--- SumOddLengthSubarrays/Program.cs
+++ SumOddLengthSubarrays/Program.cs
@@ -6,6 +6,36 @@ public class Solution
 {
     public int SumOddLengthSubarrays(int[] arr)
     {
+        // 1 4 2 5 3
+        // 1 5 7 12 15
 
+        // 1 + 4 + 2 + 5 + 3
+        //+ 7 + (12-1) + (15-5)
+        // + 15
+        var n = arr.Length;
+        var ps = new int[n];
+        ps[0] = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            ps[i] = ps[i - 1] + arr[i];
+        }
+        int res = default;
+        res += ps[n - 1];
+        int m = default;
+        for (int odd = 3; odd <= n; odd += 2)
+        {
+            for (int i = odd - 1; i < n; i++)
+            {
+                if (i - odd < 0)
+                {
+                    res += ps[i];
+                }
+                else
+                {
+                    res += (ps[i] - ps[i - odd]);
+                }
+            }
+        }
+        return res;
     }
 }
