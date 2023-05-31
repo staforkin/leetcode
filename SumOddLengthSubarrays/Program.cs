c90281ffef8d28e590385b298df74673ece7387c
staforkin <staforkin.denis@gmail.com>
2023-05-31 21:31:27 +0300

    Setup for Sum of All Odd Length Subarrays problem

---
+++ SumOddLengthSubarrays/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SumOddLengthSubarrays(new[] { 1, 4, 2, 5, 3 }));
+
+// https://leetcode.com/problems/sum-of-all-odd-length-subarrays
+public class Solution
+{
+    public int SumOddLengthSubarrays(int[] arr)
+    {
+
+    }
+}
