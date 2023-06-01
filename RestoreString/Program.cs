0cf5d097ab045b79de4e9b251330b8517450ca12
staforkin <staforkin.denis@gmail.com>
2023-06-01 20:08:10 +0300

    Setup for Shuffle String problem

---
+++ RestoreString/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RestoreString("codeleet", new[] { 4, 5, 6, 7, 0, 2, 1, 3 }));
+
+// https://leetcode.com/problems/shuffle-string
+public class Solution
+{
+    public string RestoreString(string s, int[] indices)
+    {
+
+    }
+}
