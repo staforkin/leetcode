6ad0d256f435f8ea9ac7cbdec1bd0fa03009ad34
staforkin <staforkin.denis@gmail.com>
2023-06-02 17:13:46 +0300

    Setup for Check If Two String Arrays are Equivalent problem

---
+++ ArrayStringsAreEqual/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ArrayStringsAreEqual(new[] { "ab", "c" }, new[] { "a", "bc" }));
+
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent
+public class Solution
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+
+    }
+}
