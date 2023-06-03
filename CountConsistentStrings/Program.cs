1e6212bfd54c0ab5c2f78286ef457b1fb368420c
staforkin <staforkin.denis@gmail.com>
2023-06-03 16:04:23 +0300

    Setup for Count the Number of Consistent Strings problem

---
+++ CountConsistentStrings/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CountConsistentStrings("ab", new[] { "ab", "bd", "aaab", "baa", "badab" }));
+
+// https://leetcode.com/problems/count-the-number-of-consistent-strings
+public class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+
+    }
+}
