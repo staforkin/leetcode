a30d6c1ce4c13df7e42eba51f3b174d6b6df8311
staforkin <staforkin.denis@gmail.com>
2023-06-04 19:01:31 +0300

    Number of Provinces problem

---
+++ FindCircleNum/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var isConnected = new int[][] { new int[] { 1, 1, 0 }, new int[] { 1, 1, 0 }, new int[] { 0, 0, 1 } };
+Console.WriteLine(solution.FindCircleNum(isConnected));
+
+// https://leetcode.com/problems/number-of-provinces
+public class Solution
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+
+    }
+}
