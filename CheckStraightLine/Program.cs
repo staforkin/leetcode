21231fb95d492dfeb3319b74e8132159082ffb7a
staforkin <staforkin.denis@gmail.com>
2023-06-05 21:33:45 +0300

    Setup for Check If It Is a Straight Line problem

---
+++ CheckStraightLine/Program.cs
@@ -0,0 +1,19 @@
+﻿var solution = new Solution();
+var coordinates = new int[][]{
+    new int[] { 1, 2 },
+    new int[] { 2, 3 },
+    new int[] { 3, 4 },
+    new int[] { 4, 5 },
+    new int[] { 5, 6 },
+    new int[] { 6, 7 }
+    };
+Console.WriteLine(solution.CheckStraightLine(coordinates));
+
+// https://leetcode.com/problems/check-if-it-is-a-straight-line
+public class Solution
+{
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+
+    }
+}
