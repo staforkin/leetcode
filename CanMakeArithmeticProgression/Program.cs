a76f2df58cc27ba4edbe8d4e7344de51fd51ccd4
staforkin <staforkin.denis@gmail.com>
2023-06-06 21:03:42 +0300

    Setup for Can Make Arithmetic Progression From Sequence problem

---
+++ CanMakeArithmeticProgression/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CanMakeArithmeticProgression(new[] { 3, 5, 1 }));
+
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence
+public class Solution
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+
+    }
+}
