46da7234d9cc797a9e909cdcfe143ba913c26560
staforkin <staforkin.denis@gmail.com>
2023-06-07 21:29:46 +0300

    Setup for Minimum Flips to Make a OR b Equal to c problem

---
+++ MinFlips/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinFlips(2, 6, 5));
+
+// https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c
+public class Solution
+{
+    public int MinFlips(int a, int b, int c)
+    {
+
+    }
+}
