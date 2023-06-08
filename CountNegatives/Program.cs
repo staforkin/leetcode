859bb918ca93e9aa1b079fc2902b4801fd9af823
staforkin <staforkin.denis@gmail.com>
2023-06-08 18:49:01 +0300

    Setup for Count Negative Numbers in a Sorted Matrix problem

---
+++ CountNegatives/Program.cs
@@ -0,0 +1,17 @@
+﻿var solution = new Solution();
+var grid = new int[][]{
+    new []{4,3,2,-1},
+    new []{3,2,1,-1},
+    new []{1,1,-1,-2},
+    new []{-1,-1,-2,-3}
+};
+Console.WriteLine(solution.CountNegatives(grid));
+
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix
+public class Solution
+{
+    public int CountNegatives(int[][] grid)
+    {
+
+    }
+}
