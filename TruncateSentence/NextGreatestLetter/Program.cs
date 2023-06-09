245efa9057932b8e2c24511fa441873eaf043d71
staforkin <staforkin.denis@gmail.com>
2023-06-09 17:06:47 +0300

    Setup for Find Smallest Letter Greater Than Target problem

---
+++ TruncateSentence/NextGreatestLetter/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'a'));
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target
+public class Solution
+{
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+
+    }
+}
