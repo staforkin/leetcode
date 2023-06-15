c1e216b7e307a4409a63b52f2cbcae2aca229790
staforkin <staforkin.denis@gmail.com>
2023-06-15 10:14:17 +0300

    Setup for Final Prices With a Special Discount in a Shop problem

---
+++ FinalPrices/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FinalPrices(new[] { 8, 4, 6, 2, 3 })));
+
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop
+public class Solution
+{
+    public int[] FinalPrices(int[] prices)
+    {
+
+    }
+}
