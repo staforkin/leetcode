9a8855eddce64e144a258fdbed82944585ca840e
staforkin <staforkin.denis@gmail.com>
2023-06-16 21:56:49 +0300

    Setup for Check if All Characters Have Equal Number of Occurrences problem

---
+++ AreOccurrencesEqual/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.AreOccurrencesEqual("abacbc"));
+
+// https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences
+public class Solution
+{
+    public bool AreOccurrencesEqual(string s)
+    {
+
+    }
+}
