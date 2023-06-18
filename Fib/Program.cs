0f90efa9bb925353eff62604fa20f31358fe4df3
staforkin <staforkin.denis@gmail.com>
2023-06-18 22:30:35 +0300

    Setup for Fibonacci Number problem

---
+++ Fib/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Fib(2));
+
+// https://leetcode.com/problems/fibonacci-number
+public class Solution
+{
+    public int Fib(int n)
+    {
+
+    }
+}
