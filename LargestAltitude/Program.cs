fc218fd32b2e4a2248a284deca8dd7f73e704751
staforkin <staforkin.denis@gmail.com>
2023-06-19 20:48:26 +0300

    Setup for Find the Highest Altitude problem

---
+++ LargestAltitude/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LargestAltitude(new[] { -5, 1, 5, 0, -7 }));
+
+// https://leetcode.com/problems/find-the-highest-altitude
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+
+    }
+}
