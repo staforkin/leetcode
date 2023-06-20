19fc93af7146a6b4a623a496e56abb801dfbf836
staforkin <staforkin.denis@gmail.com>
2023-06-20 19:37:06 +0300

    Setup for Decrypt String from Alphabet to Integer Mapping problem

---
+++ FreqAlphabets/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FreqAlphabets("10#11#12"));
+
+// https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping
+public class Solution
+{
+    public string FreqAlphabets(string s)
+    {
+
+    }
+}
