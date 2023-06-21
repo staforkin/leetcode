9b9e1263bc50ecdf2917781a27b77d6865bf53c2
staforkin <staforkin.denis@gmail.com>
2023-06-21 20:58:18 +0300

    Setup for N-Repeated Element in Size 2N Array problem

---
+++ RepeatedNTimes/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RepeatedNTimes(new[] { 1, 2, 3, 3 }));
+
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array
+public class Solution
+{
+    public int RepeatedNTimes(int[] nums)
+    {
+
+    }
+}
