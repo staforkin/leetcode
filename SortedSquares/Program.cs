8fcc98ff1a4704dfb163eef47fc8bab3e832dd3f
staforkin <staforkin.denis@gmail.com>
2023-06-22 19:56:58 +0300

    Setup for Squares of a Sorted Array problem

---
+++ SortedSquares/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SortedSquares(new[] { -4, -1, 0, 3, 10 })));
+
+// https://leetcode.com/problems/squares-of-a-sorted-array
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+
+    }
+}
