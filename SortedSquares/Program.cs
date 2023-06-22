83412e2a5757b1836e23eaf0a18a448d7cf3fa2e
staforkin <staforkin.denis@gmail.com>
2023-06-22 20:12:28 +0300

    Squares of a Sorted Array problem

--- SortedSquares/Program.cs
+++ SortedSquares/Program.cs
@@ -7,5 +7,23 @@ public class Solution
     public int[] SortedSquares(int[] nums)
     {
 
+        // -4 -1 0 3 10
+        int n = nums.Length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--)
+        {
+            if (Math.Abs(nums[i]) > Math.Abs(nums[j]))
+            {
+                result[p] = nums[i] * nums[i];
+                i++;
+            }
+            else
+            {
+                result[p] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return result;
     }
 }
