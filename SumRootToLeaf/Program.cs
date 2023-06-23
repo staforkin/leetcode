1249b8f989e35191fcf7e740c226b7b4efe5679e
staforkin <staforkin.denis@gmail.com>
2023-06-23 23:50:21 +0300

    Setup for Sum of Root To Leaf Binary Numbers problem

---
+++ SumRootToLeaf/Program.cs
@@ -0,0 +1,30 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.right = new TreeNode(1);
+root.right.left = new TreeNode(0);
+root.right.right = new TreeNode(1);
+root.left = new TreeNode(0);
+root.left.left = new TreeNode(0);
+root.left.right = new TreeNode(1);
+
+//https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers
+public class Solution
+{
+    public int SumRootToLeaf(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
