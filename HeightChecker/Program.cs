ecd63f0ba45c469c6f010cdca89931f07d2339b7
staforkin <staforkin.denis@gmail.com>
2023-06-24 17:04:33 +0300

    Setup for Height Checker problem

---
+++ HeightChecker/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.HeightChecker(new[] { 1, 1, 4, 2, 1, 3 }));
+
+// https://leetcode.com/problems/height-checker
+public class Solution
+{
+    public int HeightChecker(int[] heights)
+    {
+
+    }
+}
