5334cedc960b2f686df34ed753eb0d67728efeff
staforkin <staforkin.denis@gmail.com>
2023-06-25 16:15:03 +0300

    Setup for Find Common Characters problem

---
+++ CommonChars/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.CommonChars(new[] { "bella", "label", "roller" }).ToArray()));
+
+// https://leetcode.com/problems/find-common-characters
+public class Solution
+{
+    public IList<string> CommonChars(string[] words)
+    {
+
+    }
+}
