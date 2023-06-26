dd4d9b7198ee57c6bd6232a1bb33bc808abae5aa
staforkin <staforkin.denis@gmail.com>
2023-06-26 20:58:49 +0300

    Setup for Regions Cut By Slashes problem

---
+++ RegionsBySlashes/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RegionsBySlashes(new[] { " /", "/ " }));
+
+// https://leetcode.com/problems/regions-cut-by-slashes
+public class Solution
+{
+    public int RegionsBySlashes(string[] grid)
+    {
+        return 0;
+    }
+}
