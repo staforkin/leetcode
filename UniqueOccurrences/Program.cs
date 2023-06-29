af0b98174b77cd55b2ff2ebbc36c311ba35a44da
staforkin <staforkin.denis@gmail.com>
2023-06-29 21:44:06 +0300

    Setup for Unique Number of Occurrences problem

---
+++ UniqueOccurrences/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.UniqueOccurrences(new[] { 1, 2, 2, 1, 1, 3 }));
+
+// https://leetcode.com/problems/unique-number-of-occurrences/
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+
+    }
+}
