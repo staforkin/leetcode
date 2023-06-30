9ff130cc340d889a959e924b00077c9c0e9b2ad5
staforkin <staforkin.denis@gmail.com>
2023-06-30 21:05:08 +0300

    Setup for Find Numbers with Even Number of Digits problem

---
+++ FindNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindNumbers(new[] { 12, 345, 2, 6, 7896 }));
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits
+public class Solution
+{
+    public int FindNumbers(int[] nums)
+    {
+
+    }
+}
