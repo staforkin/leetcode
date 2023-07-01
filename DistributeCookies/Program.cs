ffd4fb959abe6de25d3973688b48bd3c3291ae1f
staforkin <staforkin.denis@gmail.com>
2023-07-01 19:19:34 +0300

    Setup for Fair Distribution of Cookies

---
+++ DistributeCookies/Program.cs
@@ -0,0 +1,9 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.DistributeCookies(new []{8,15,10,20,8},2));
+
+// https://leetcode.com/problems/fair-distribution-of-cookies
+public class Solution {
+    public int DistributeCookies(int[] cookies, int k) {
+        
+    }
+}
