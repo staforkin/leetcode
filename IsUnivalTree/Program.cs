6693fb805d6eeb433e3eef96c6df6d1c0bb36a9f
staforkin <staforkin.denis@gmail.com>
2023-07-02 22:40:29 +0300

    Setup for Univalued Binary Tree problem

---
+++ IsUnivalTree/Program.cs
@@ -0,0 +1,27 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(1);
+root.right = new TreeNode(1);
+Console.WriteLine(solution.IsUnivalTree(root));
+
+// https://leetcode.com/problems/univalued-binary-tree
+public class Solution
+{
+    public bool IsUnivalTree(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
