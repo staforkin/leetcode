6562148f50da1fc76c288561e9263f617629fb17
staforkin <staforkin.denis@gmail.com>
2023-07-03 19:31:49 +0300

    Setup for Split a String in Balanced Strings problem

---
+++ BalancedStringSplit/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.BalancedStringSplit("RLRRLLRLRL"));
+
+// https://leetcode.com/problems/split-a-string-in-balanced-strings
+public class Solution
+{
+    public int BalancedStringSplit(string s)
+    {
+
+    }
+}
