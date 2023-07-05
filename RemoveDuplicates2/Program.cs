82b2a0419010dee224fbefa91bb01af3f600698f
staforkin <staforkin.denis@gmail.com>
2023-07-05 21:29:33 +0300

    Setup for Remove All Adjacent Duplicates In String problem

---
+++ RemoveDuplicates2/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RemoveDuplicates("abbaca"));
+
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string
+public class Solution
+{
+    public string RemoveDuplicates(string s)
+    {
+
+    }
+}
