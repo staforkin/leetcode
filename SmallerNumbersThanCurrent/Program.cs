a93b4e7a512bdd2940e52dc1a80b6a413d52461c
staforkin <staforkin.denis@gmail.com>
2023-07-06 20:15:22 +0300

    Setup for How Many Numbers Are Smaller Than the Current Number problem

---
+++ SmallerNumbersThanCurrent/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SmallerNumbersThanCurrent(new[] { 8, 1, 2, 2, 3 })));
+
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number
+public class Solution
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+
+    }
+}
