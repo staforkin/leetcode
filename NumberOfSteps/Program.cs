18dcc94b165f866260ed00ed8636051338e3fa3b
staforkin <staforkin.denis@gmail.com>
2023-07-06 20:02:14 +0300

    Setup for Number of Steps to Reduce a Number to Zero problem

---
+++ NumberOfSteps/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumberOfSteps(14));
+
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
+public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+
+    }
+}
