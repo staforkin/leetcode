5d7058341c842d6bf1fb1d16b2fe1bd039512534
staforkin <staforkin.denis@gmail.com>
2023-07-08 22:37:47 +0300

    Setup for Maximum 69 Number problem

---
+++ Maximum69Number/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.Maximum69Number(9669));
+
+// https://leetcode.com/problems/maximum-69-number
+public class Solution
+{
+    public int Maximum69Number(int num)
+    {
+
+    }
+}
