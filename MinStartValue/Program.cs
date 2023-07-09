af3b8c16e28b747de53c567a9a1c512d96426d18
staforkin <staforkin.denis@gmail.com>
2023-07-09 11:46:56 +0300

    Setup for Minimum Value to Get Positive Step by Step Sum problem

---
+++ MinStartValue/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MinStartValue(new[] { -3, 2, -3, 4, 2 }));
+
+// https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum
+public class Solution
+{
+    public int MinStartValue(int[] nums)
+    {
+
+    }
+}
