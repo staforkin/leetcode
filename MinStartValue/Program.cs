644fa5bccacc43249b2cbc242b3b1e9cef9c6086
staforkin <staforkin.denis@gmail.com>
2023-07-09 12:07:04 +0300

    Minimum Value to Get Positive Step by Step Sum problem

--- MinStartValue/Program.cs
+++ MinStartValue/Program.cs
@@ -6,6 +6,19 @@ public class Solution
 {
     public int MinStartValue(int[] nums)
     {
-
+        int start = 1;
+        int sum = start;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            //Console.WriteLine($"sum is {sum}");
+            if (sum < 1)
+            {
+                start = start + (1 - sum);
+                sum += (1 - sum);
+                //Console.WriteLine($"  new start is {start}");
+            }
+        }
+        return start;
     }
 }
--- README.md
+++ README.md
@@ -292,3 +292,4 @@ List of problems solved:
 - [Number of Steps to Reduce a Number to Zero](https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero)
 - [How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number)
 - [Maximum 69 Number](https://leetcode.com/problems/maximum-69-number)
+- [Minimum Value to Get Positive Step by Step Sum](https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum)
