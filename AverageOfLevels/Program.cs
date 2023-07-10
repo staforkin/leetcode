b539499918abf8c175f0c047d559da72ccef8ea9
staforkin <staforkin.denis@gmail.com>
2023-07-10 20:58:25 +0300

    Average of Levels in Binary Tree problem

--- AverageOfLevels/Program.cs
+++ AverageOfLevels/Program.cs
@@ -1,7 +1,7 @@
 ﻿var solution = new Solution();
-var root = new TreeNode(3);
-root.left = new TreeNode(9);
-root.right = new TreeNode(20);
+var root = new TreeNode(2147483647);
+root.left = new TreeNode(2147483647);
+root.right = new TreeNode(2147483647);
 root.right.right = new TreeNode(7);
 root.right.left = new TreeNode(15);
 foreach (var item in solution.AverageOfLevels(root))
@@ -14,7 +14,29 @@ public class Solution
 {
     public IList<double> AverageOfLevels(TreeNode root)
     {
-
+        var res = new List<double>();
+        var q = new Queue<TreeNode>();
+        q.Enqueue(root);
+        while (q.Any())
+        {
+            var cnt = q.Count();
+            long sum = default;
+            for (int i = 0; i < cnt; i++)
+            {
+                var t = q.Dequeue();
+                if (t.left != null)
+                {
+                    q.Enqueue(t.left);
+                }
+                if (t.right != null)
+                {
+                    q.Enqueue(t.right);
+                }
+                sum += t.val;
+            }
+            res.Add((double)sum / cnt);
+        }
+        return res;
     }
 }
 
--- README.md
+++ README.md
@@ -293,3 +293,4 @@ List of problems solved:
 - [How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number)
 - [Maximum 69 Number](https://leetcode.com/problems/maximum-69-number)
 - [Minimum Value to Get Positive Step by Step Sum](https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum)
+- [Average of Levels in Binary Tree](https://leetcode.com/problems/average-of-levels-in-binary-tree)
