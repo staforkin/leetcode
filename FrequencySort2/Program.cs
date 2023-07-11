82df0573563c4baec0b68c06d3dc1929b6d59631
staforkin <staforkin.denis@gmail.com>
2023-07-11 20:11:33 +0300

    Setup for Sort Array by Increasing Frequency problem

---
+++ FrequencySort2/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FrequencySort(new[] { 1, 1, 2, 2, 2, 3 })));
+
+// https://leetcode.com/problems/sort-array-by-increasing-frequency
+public class Solution
+{
+    public int[] FrequencySort(int[] nums)
+    {
+
+    }
+}
