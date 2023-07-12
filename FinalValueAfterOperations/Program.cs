f780e166ec280f9d7374a629d994c4ccea52bc69
staforkin <staforkin.denis@gmail.com>
2023-07-12 20:50:44 +0300

    Setup for Final Value of Variable After Performing Operations problem

---
+++ FinalValueAfterOperations/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FinalValueAfterOperations(new[] { "--X", "X++", "X++" }));
+
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations
+public class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+
+    }
+}
