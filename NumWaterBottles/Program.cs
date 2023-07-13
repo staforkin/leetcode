265bacdadebd528498f6ff9e261dfb855c8a2375
staforkin <staforkin.denis@gmail.com>
2023-07-13 19:47:02 +0300

    Setup for Water Bottles problem

---
+++ NumWaterBottles/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.NumWaterBottles(9, 3));
+
+// https://leetcode.com/problems/water-bottles
+public class Solution
+{
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+
+    }
+}
