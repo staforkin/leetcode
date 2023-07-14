a95aeb87e799349e6b8581e5a13324394b5d8da8
staforkin <staforkin.denis@gmail.com>
2023-07-14 21:41:49 +0300

    Setup for Special Array With X Elements Greater Than or Equal X problem

---
+++ SpecialArray/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.SpecialArray(new[] { 3, 5 }));
+
+// https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x
+public class Solution
+{
+    public int SpecialArray(int[] nums)
+    {
+
+    }
+}
