a0ec705fb7abc3b00c35484ff21d56335ebbdb73
staforkin <staforkin.denis@gmail.com>
2023-07-15 22:13:13 +0300

    Setup for Counting Words With a Given Prefix problem

---
+++ PrefixCount/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.PrefixCount(new[] { "pay", "attention", "practice", "attend" }, "at"));
+
+// https://leetcode.com/problems/counting-words-with-a-given-prefix
+public class Solution
+{
+    public int PrefixCount(string[] words, string pref)
+    {
+
+    }
+}
