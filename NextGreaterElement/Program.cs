665825fdb1ff56363adbc0bb5c1525e83bc4f8b6
staforkin <staforkin.denis@gmail.com>
2023-07-16 17:28:59 +0300

    Setup for Next Greater Element I problem

---
+++ NextGreaterElement/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.NextGreaterElement(new[] { 4, 1, 2 }, new[] { 1, 3, 4, 2 })));
+
+// https://leetcode.com/problems/next-greater-element-i/
+public class Solution
+{
+    public int[] NextGreaterElement(int[] nums1, int[] nums2)
+    {
+
+    }
+}
