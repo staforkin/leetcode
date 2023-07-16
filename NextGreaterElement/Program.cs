4185eb53ab5d60cb69cdaa5b07ad30626f537acf
staforkin <staforkin.denis@gmail.com>
2023-07-16 17:42:24 +0300

    Next Greater Element I problem

--- NextGreaterElement/Program.cs
+++ NextGreaterElement/Program.cs
@@ -6,6 +6,23 @@ public class Solution
 {
     public int[] NextGreaterElement(int[] nums1, int[] nums2)
     {
+        // 4 1 2
+        // 1 3 4 2
 
+        var map = new Dictionary<int, int>(); // map from x to next greater element of x
+        var stack = new Stack<int>();
+        foreach (int num in nums2)
+        {
+            while (stack.Any() && stack.Peek() < num)
+            {
+                map.Add(stack.Pop(), num);
+            }
+            stack.Push(num);
+        }
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            nums1[i] = map.GetValueOrDefault(nums1[i], -1);
+        }
+        return nums1;
     }
 }
--- README.md
+++ README.md
@@ -299,3 +299,4 @@ List of problems solved:
 - [Water Bottles](https://leetcode.com/problems/water-bottles)
 - [Special Array With X Elements Greater Than or Equal X](https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x)
 - [Counting Words With a Given Prefix](https://leetcode.com/problems/counting-words-with-a-given-prefix)
+- [Next Greater Element I](https://leetcode.com/problems/next-greater-element-i)
