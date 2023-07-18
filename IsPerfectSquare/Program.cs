12aae7ecfc0ed89aff4f2ad5a393ca559059c4a1
staforkin <staforkin.denis@gmail.com>
2023-07-18 21:22:23 +0300

    Setup for Valid Perfect Square problem

---
+++ IsPerfectSquare/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsPerfectSquare(16));
+
+// https://leetcode.com/problems/valid-perfect-square
+public class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+
+    }
+}
