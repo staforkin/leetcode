cde5380adde142c38b317ae05b725fa7d303f4c5
staforkin <staforkin.denis@gmail.com>
2023-07-19 21:13:07 +0300

    Setup for Find the Difference problem

---
+++ FindTheDifference/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindTheDifference("abcd", "abcde"));
+
+// https://leetcode.com/problems/find-the-difference
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+
+    }
+}
