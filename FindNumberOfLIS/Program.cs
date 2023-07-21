089151225c4f35aa1108bed4ee8efe220941a422
staforkin <staforkin.denis@gmail.com>
2023-07-21 17:50:09 +0300

    Setup for Number of Longest Increasing Subsequence problem

---
+++ FindNumberOfLIS/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindNumberOfLIS(new[] { 1, 3, 5, 4, 7 }));
+
+// https://leetcode.com/problems/number-of-longest-increasing-subsequence
+public class Solution
+{
+    public int FindNumberOfLIS(int[] nums)
+    {
+
+    }
+}
