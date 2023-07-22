32f4296876b148409d6d9ad5b01809d4a79cc06e
staforkin <staforkin.denis@gmail.com>
2023-07-22 18:30:36 +0300

    Setup for Assign Cookies problem

---
+++ FindContentChildren/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindContentChildren(new[] { 1, 2, 3 }, new[] { 1, 1 }));
+
+// https://leetcode.com/problems/assign-cookies/
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+
+    }
+}
