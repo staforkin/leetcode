ea05c718ee5e093e2a952f5d684fdf5fc8ac17c1
staforkin <staforkin.denis@gmail.com>
2023-07-23 15:52:47 +0300

    Setup for All Possible Full Binary Trees problem

---
+++ AllPossibleFBT/Program.cs
@@ -0,0 +1,25 @@
+﻿var solution = new Solution();
+var roots = solution.AllPossibleFBT(7);
+Console.WriteLine(roots.Count);
+
+// https://leetcode.com/problems/all-possible-full-binary-trees
+public class Solution
+{
+    public IList<TreeNode> AllPossibleFBT(int n)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
