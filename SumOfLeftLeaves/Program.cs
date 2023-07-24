6961e88e9b3e9b51ef25946fc81b3fc280d2b24c
staforkin <staforkin.denis@gmail.com>
2023-07-24 21:27:53 +0300

    Setup for Sum of Left Leaves problem

---
+++ SumOfLeftLeaves/Program.cs
@@ -0,0 +1,29 @@
+﻿var solution = new Solution();
+var root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+Console.WriteLine(solution.SumOfLeftLeaves(root));
+
+// https://leetcode.com/problems/sum-of-left-leaves
+public class Solution
+{
+    public int SumOfLeftLeaves(TreeNode root)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
