29305407baf7e4e8d12d5e6ad7d9c4451e6b2e2f
staforkin <staforkin.denis@gmail.com>
2023-07-25 23:36:45 +0300

    Setup for Island Perimeter problem

---
+++ IslandPerimeter/Program.cs
@@ -0,0 +1,12 @@
+﻿var solution = new Solution();
+var grid = new int[][] { new[] { 0, 1, 0, 0 }, new[] { 1, 1, 1, 0 }, new[] { 0, 1, 0, 0 }, new[] { 1, 1, 0, 0 } };
+Console.WriteLine(solution.IslandPerimeter(grid));
+
+// https://leetcode.com/problems/island-perimeter
+public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+
+    }
+}
