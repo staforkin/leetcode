7488feda7f7ce32272435cbbc38f71eb58a4efa4
staforkin <staforkin.denis@gmail.com>
2023-07-28 23:39:14 +0300

    Setup for Relative Ranks problem

---
+++ FindRelativeRanks/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.FindRelativeRanks(new[] { 5, 4, 3, 2, 1 }))
+{
+    Console.WriteLine(item);
+}
+
+// https://leetcode.com/problems/relative-ranks
+public class Solution
+{
+    public string[] FindRelativeRanks(int[] score)
+    {
+
+    }
+}
