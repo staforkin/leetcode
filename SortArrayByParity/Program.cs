e510ce6739f51a7c31bae2034058b9784e75195f
staforkin <staforkin.denis@gmail.com>
2023-07-29 19:24:03 +0300

    Setup for Sort Array By Parity problem

---
+++ SortArrayByParity/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SortArrayByParity(new[] { 3, 1, 2, 4 })));
+
+// https://leetcode.com/problems/sort-array-by-parity
+public class Solution
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+
+    }
+}
