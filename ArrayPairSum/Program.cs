8ac155cf06414f024f2b1c2936c04fe98deb5d18
staforkin <staforkin.denis@gmail.com>
2023-07-31 21:11:25 +0300

    Setup for Array Partition problem

---
+++ ArrayPairSum/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ArrayPairSum(new[] { 1, 4, 3, 2 }));
+
+// https://leetcode.com/problems/array-partition
+public class Solution
+{
+    public int ArrayPairSum(int[] nums)
+    {
+
+    }
+}
