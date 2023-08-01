54a1d2f0372a776a960475e7f4700cea8b9dd0ca
staforkin <staforkin.denis@gmail.com>
2023-08-01 21:01:14 +0300

    Setup for Minimum Index Sum of Two Lists problem

---
+++ FindRestaurant/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindRestaurant(new[] { "Shogun", "Tapioca Express", "Burger King", "KFC" }, new[] { "Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun" }));
+
+// https://leetcode.com/problems/minimum-index-sum-of-two-lists
+public class Solution
+{
+    public string[] FindRestaurant(string[] list1, string[] list2)
+    {
+
+    }
+}
