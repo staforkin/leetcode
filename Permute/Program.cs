ce69f8eb53012c15168c3472841b75f310127003
staforkin <staforkin.denis@gmail.com>
2023-08-02 20:34:56 +0300

    Second solution for Permutations problem

--- Permute/Program.cs
+++ Permute/Program.cs
@@ -1,5 +1,5 @@
 ﻿var solution = new Solution();
-foreach (var item in solution.Permute(new[] { 1, 2, 3 }))
+foreach (var item in solution.Permute2(new[] { 1, 2, 3 }))
 {
     Console.WriteLine(string.Join(",", item));
 }
@@ -7,11 +7,19 @@ foreach (var item in solution.Permute(new[] { 1, 2, 3 }))
 //https://leetcode.com/problems/permutations/
 public class Solution
 {
+    private List<IList<int>> _result = new();
     public IList<IList<int>> Permute(int[] nums)
     {
         var result = new List<IList<int>>();
         backTrack(result, new List<int>(),new HashSet<int>(), nums);
         return result;
+    }
+
+    public IList<IList<int>> Permute2(int[] nums)
+    {
+        _result = new();
+        MakePermutations(nums, 0, new int[nums.Length]);
+        return _result;
     }
 
     private void backTrack(List<IList<int>> lists, List<int> tempList, HashSet<int> tmpSet, int[] nums)
@@ -35,6 +43,26 @@ public class Solution
                 tempList.RemoveAt(tempList.Count() - 1);
                 tmpSet.Remove(t);
             }
+        }
+    }
+
+    private void MakePermutations(int[] nums, int position, int[] permutation)
+    {
+        if (position == nums.Length)
+        {
+            _result.Add(new List<int>(permutation));
+            return;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (Array.IndexOf(permutation, nums[i], 0, position) != -1)
+            {
+                continue;
+            }
+
+            permutation[position] = nums[i];
+            MakePermutations(nums, position + 1, permutation);
         }
     }
 }
