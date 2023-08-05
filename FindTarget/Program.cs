0c07b06ea69edaec2934e2c795ba5e0cffa3f999
staforkin <staforkin.denis@gmail.com>
2023-08-05 11:18:21 +0300

    Setup for Two Sum IV - Input is a BST problem

---
+++ FindTarget/Program.cs
@@ -0,0 +1,27 @@
+﻿var solution = new Solution();
+var root = new TreeNode(5);
+root.left = new TreeNode(3);
+root.right = new TreeNode(6);
+Console.WriteLine(solution.FindTarget(root, 9));
+
+// https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+public class Solution
+{
+    public bool FindTarget(TreeNode root, int k)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
