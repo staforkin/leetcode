2f0430a9ed09ee5252fd1f2067074409229c426e
staforkin <staforkin.denis@gmail.com>
2023-08-06 23:11:54 +0300

    Setup for Rotate String problem

---
+++ RotateString/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RotateString("abcde", "cdeab"));
+
+// https://leetcode.com/problems/rotate-string/
+public class Solution
+{
+    public bool RotateString(string s, string goal)
+    {
+
+    }
+}
