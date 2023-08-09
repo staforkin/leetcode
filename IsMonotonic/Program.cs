40228227e2f63a40aac0917c1a79eb7dd3c237af
staforkin <staforkin.denis@gmail.com>
2023-08-09 08:54:18 +0300

    Monotonic Array problem

---
+++ IsMonotonic/Program.cs
@@ -0,0 +1,18 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsMonotonic(new[] { 1, 2, 2, 3 }));
+
+// https://leetcode.com/problems/monotonic-array/
+public class Solution
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        var inc = true;
+        var dec = true;
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            inc &= nums[i - 1] <= nums[i];
+            dec &= nums[i - 1] >= nums[i];
+        }
+        return inc || dec;
+    }
+}
