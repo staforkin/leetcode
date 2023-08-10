b288e164efd41bd2c418f6664db4b7cde23dd579
staforkin <staforkin.denis@gmail.com>
2023-08-10 09:13:44 +0300

    Setup for Fair Candy Swap problem

---
+++ FairCandySwap/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.FairCandySwap(new[] { 1, 1 }, new[] { 2, 2 })));
+
+// https://leetcode.com/problems/fair-candy-swap/
+public class Solution
+{
+    public int[] FairCandySwap(int[] aliceSizes, int[] bobSizes)
+    {
+
+    }
+}
