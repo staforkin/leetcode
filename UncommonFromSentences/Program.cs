20718a774ad96a5da914b748f9c36fbd90ed24c5
staforkin <staforkin.denis@gmail.com>
2023-08-11 22:16:45 +0300

    Setup for Uncommon Words from Two Sentences problem

---
+++ UncommonFromSentences/Program.cs
@@ -0,0 +1,14 @@
+﻿var solution = new Solution();
+foreach (var item in solution.UncommonFromSentences("this apple is sweet", "this apple is sour")) ;
+{
+    Console.WriteLine(item);
+}
+
+// https://leetcode.com/problems/uncommon-words-from-two-sentences/
+public class Solution
+{
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+
+    }
+}
