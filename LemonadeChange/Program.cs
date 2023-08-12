ffb63402a8aaf96e474d7101634adaeb302f4370
staforkin <staforkin.denis@gmail.com>
2023-08-12 08:05:52 +0300

    Setup for Lemonade Change problem

---
+++ LemonadeChange/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.LemonadeChange(new[] { 5, 5, 5, 10, 20 }));
+
+// https://leetcode.com/problems/lemonade-change/
+public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        
+    }
+}
