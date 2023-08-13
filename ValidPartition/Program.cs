70109b5a765fea8a6bdee6461512f8f3a0b567b4
staforkin <staforkin.denis@gmail.com>
2023-08-13 12:21:35 +0300

    Setup for Check if There is a Valid Partition For The Array problem

---
+++ ValidPartition/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.ValidPartition(new[] { 4, 4, 4, 5, 6 }));
+
+// https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+public class Solution
+{
+    public bool ValidPartition(int[] nums)
+    {
+
+    }
+}
