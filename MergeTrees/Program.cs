e54119e64c04a3c1db5aa85b68bb638e24689dc3
staforkin <staforkin.denis@gmail.com>
2023-08-14 20:24:20 +0300

    Setup for Merge Two Binary Trees problem

---
+++ MergeTrees/Program.cs
@@ -0,0 +1,34 @@
+﻿var solution = new Solution();
+var root = new TreeNode(1);
+root.left = new TreeNode(3);
+root.left.left = new TreeNode(5);
+root.right = new TreeNode(2);
+
+var root2 = new TreeNode(2);
+root2.left = new TreeNode(1);
+root2.left.right = new TreeNode(4);
+root2.right = new TreeNode(3);
+root2.right.right = new TreeNode(7);
+Console.WriteLine(solution.MergeTrees(root, root2));
+
+// https://leetcode.com/problems/merge-two-binary-trees/
+public class Solution
+{
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
