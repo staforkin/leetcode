e60801951009553d103abd527119c223bebe784d
staforkin <staforkin.denis@gmail.com>
2023-08-15 23:16:36 +0300

    Setup for Longest Harmonious Subsequence problem

---
+++ FindLHS/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.FindLHS(new[] { 1, 3, 2, 2, 5, 2, 3, 7 }));
+
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+public class Solution
+{
+    public int FindLHS(int[] nums)
+    {
+
+    }
+}
