eb98d43c601b608c33faf7e2a9d11259f69f80e9
staforkin <staforkin.denis@gmail.com>
2023-08-16 21:09:52 +0300

    Setup for Repeated Substring Pattern problem

---
+++ RepeatedSubstringPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.RepeatedSubstringPattern("abab"));
+
+// https://leetcode.com/problems/repeated-substring-pattern/
+public class Solution
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+
+    }
+}
