210b52127cc30dead76dd441b30ac850ba2f7452
staforkin <staforkin.denis@gmail.com>
2023-08-17 21:34:19 +0300

    Setup for Maximum Product of Three Numbers problem

---
+++ MaximumProduct/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.MaximumProduct(new[] { 1, 2, 3 }));
+
+// https://leetcode.com/problems/maximum-product-of-three-numbers
+public class Solution
+{
+    public int MaximumProduct(int[] nums)
+    {
+
+    }
+}
