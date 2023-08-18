eefc703a65e4c51fd4a6349a67f45282ffd1e774
staforkin <staforkin.denis@gmail.com>
2023-08-18 21:27:46 +0300

    Setup for Self Dividing Numbers problem

---
+++ SelfDividingNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(string.Join(",", solution.SelfDividingNumbers(1, 22)));
+
+// https://leetcode.com/problems/self-dividing-numbers/
+public class Solution
+{
+    public IList<int> SelfDividingNumbers(int left, int right)
+    {
+
+    }
+}
