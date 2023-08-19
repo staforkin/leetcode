c1c7ed7262e1cfff6c335bbb567e87e6c7624391
staforkin <staforkin.denis@gmail.com>
2023-08-19 19:01:02 +0300

    Setup for Ransom Note problem

---
+++ CanConstruct/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.CanConstruct("aa", "aab"));
+
+// https://leetcode.com/problems/ransom-note
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+
+    }
+}
