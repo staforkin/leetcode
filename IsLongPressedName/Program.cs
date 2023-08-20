707926a9e5494e21426ad80aeee6929dad11c912
staforkin <staforkin.denis@gmail.com>
2023-08-20 19:52:36 +0300

    Setup for Long Pressed Name problem

---
+++ IsLongPressedName/Program.cs
@@ -0,0 +1,11 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.IsLongPressedName("alex", "aaleex"));
+
+// https://leetcode.com/problems/long-pressed-name
+public class Solution
+{
+    public bool IsLongPressedName(string name, string typed)
+    {
+
+    }
+}
