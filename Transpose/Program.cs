e5a129de52d3a2465fc13353c0f248969069f62f
staforkin <staforkin.denis@gmail.com>
2023-08-22 20:19:27 +0300

    Setup for Transpose Matrix problem

---
+++ Transpose/Program.cs
@@ -0,0 +1,18 @@
+﻿var solution = new Solution();
+var matrix = new int[][]{
+    new []{1,2,3},
+    new []{4,5,6},
+    new []{7,8,9}};
+foreach (var item in solution.Transpose(matrix))
+{
+    Console.WriteLine(string.Join(",", item));
+}
+
+// https://leetcode.com/problems/transpose-matrix/
+public class Solution
+{
+    public int[][] Transpose(int[][] matrix)
+    {
+
+    }
+}
